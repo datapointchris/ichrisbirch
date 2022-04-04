24e67033493d32681e575334f42bc738e4e31c6c
Chris Birch <datapointchris@gmail.com>
2022-04-03 22:22:24 -0500

    Change html layout to use jinja templates
    Complete task, delete task, add task are all working

--- euphoria/priorities/models.py
+++ euphoria/priorities/models.py
@@ -25,3 +25,10 @@ class Task(db.Model):
 
     def __repr__(self):
         return f'Task(name = {self.name}, priority = {self.priority}, category = {self.category}, subcategory1 = {self.subcategory1}, subcategory2 = {self.subcategory2}, , add_date = {self.add_date}, complete_date = {self.complete_date})'
+
+    @property
+    def time_to_complete(self):
+        if self.complete_date:
+            return f'{(self.complete_date - self.add_date).days + 1} days'
+        else:
+            return 'Task Not Completed'
--- euphoria/priorities/routes.py
+++ euphoria/priorities/routes.py
@@ -31,7 +31,7 @@ def add_fake_tasks(number):
 
 
 @priorities_bp.route('/', methods=['GET'])
-def priorities():
+def priority():
     # add_fake_tasks(10)
     top_5_tasks = (
         Task.query.filter(Task.complete_date.is_(None))
@@ -39,45 +39,31 @@ def priorities():
         .limit(5)
         .all()
     )
-    # print(top_5_tasks)
-
-    return render_template('priorities.html', top_5_tasks=top_5_tasks)
+    return render_template('priority.html', top_5_tasks=top_5_tasks)
 
 
 @priorities_bp.route('/add/', methods=['POST'])
 def add_task():
     db.session.add(Task(**request.form))
     db.session.commit()
-    return redirect(url_for('priorities_bp.priorities'))
+    return redirect(url_for('priorities_bp.priority'))
 
 
 # TODO: Make this do the completed task
 @priorities_bp.route('/complete/', methods=['POST'])
 def complete_task():
     task = Task.query.filter_by(id=request.form.get('id')).first()
-    db.session.execute(sqlalchemy.update(Task).where(Task.id == task.id).values(**task))
+    task.complete_date = datetime.now()
     db.session.commit()
-    return redirect(url_for('priorities_bp.priorities'))
-
-
-@priorities_bp.route('/update/', methods=['POST'])
-def update_task():
-    task = Task.query.filter_by(id=request.form.get('id')).first()
-    db.session.execute(sqlalchemy.update(Task).where(Task.id == task.id).values(**task))
-    db.session.commit()
-    return redirect(url_for('priorities_bp.priorities'))
+    return redirect(url_for('priorities_bp.priority'))
 
 
 @priorities_bp.route('/delete/', methods=['POST'])
 def delete_task():
-    print('DELETE DELETE')
-    print(request.form.get('id'))
     task = Task.query.filter_by(id=request.form.get('id')).first()
-    print('TASK')
-    print(task)
     db.session.execute(sqlalchemy.delete(Task).where(Task.id == task.id))
     db.session.commit()
-    return redirect(url_for('priorities_bp.priorities'))
+    return redirect(url_for('priorities_bp.priority'))
 
 
 @priorities_bp.route('/tasks/')
@@ -94,7 +80,7 @@ def tasks():
 def completed():
     completed = (
         Task.query.filter(Task.complete_date.is_not(None))
-        .order_by(Task.complete_date.asc())
+        .order_by(Task.complete_date.desc())
         .all()
     )
     return render_template('completed.html', completed=completed)
