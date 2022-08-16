94a103e495075de60cc775d75003b45815ffdab6
Chris Birch <datapointchris@gmail.com>
2022-08-16 18:03:53 -0500

    chore: Moved folder structure, forgot to commit

--- euphoria/database/sqlalchemy.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, declarative_base
-
-engine = create_engine('postgresql:///euphoria', echo=False, future=True)
-session = Session(engine, future=True)
-
-Base = declarative_base()
-Base.metadata.create_all(bind=engine)
