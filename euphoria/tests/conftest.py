9017b6a5517a86217a29d19d121a3c9a0595a1a4
Chris Birch <datapointchris@gmail.com>
2022-08-23 09:34:09 -0500

    feat: Add Docker for unit tests

--- euphoria/tests/conftest.py
+++ euphoria/tests/conftest.py
@@ -1,13 +1,14 @@
 from typing import Any
 from typing import Generator
-
+import time
 import pytest
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine, event
 import sqlalchemy
+import random
 from sqlalchemy.orm import sessionmaker, Session
-from sqlalchemy.sql.expression import text
+from sqlalchemy.schema import CreateSchema
 import docker
 
 import sys
@@ -15,10 +16,12 @@ import os
 
 from backend.common.db.sqlalchemy.session import sqlalchemy_session
 from backend.common.db.sqlalchemy.base_class import Base
+from backend.common.models.tasks import Task
 from backend.api.endpoints import tasks
 
 
-def run_postgres_in_docker():
+
+def create_postgres_in_docker():
     """Create a postgres test database in docker for unit tests
 
     Port number needs to be something different from the default in case
@@ -26,11 +29,9 @@ def run_postgres_in_docker():
 
     `auto_remove` will destroy the container once it is stopped.
     Use `testdb.stop()` to stop the container after testing
-
-
     """
-    client = docker.DockerClient()
-    testdb = client.containers.run(
+    docker_client = docker.DockerClient()
+    testdb = docker_client.containers.run(
         'postgres:14',
         detach=True,
         environment={
@@ -46,67 +47,77 @@ def run_postgres_in_docker():
     return testdb
 
 
-def start_application():
+def create_app_with_routes():
     app = FastAPI()
     app.include_router(tasks.router)
     return app
 
 
-@pytest.fixture(scope="function")
-def app() -> Generator[FastAPI, Any, None]:
-    """
-    Create a fresh database on each test case.
-    """
-
-    _app = start_application()
-    yield _app
-    Base.metadata.drop_all(engine)
-
-
-# @pytest.fixture(scope="function")
-# def db_session(app: FastAPI) -> Generator[SessionTesting, Any, None]:
-#     connection = engine.connect()
-#     transaction = connection.begin()
-#     session = SessionTesting(bind=connection)
-#     yield session  # use the session in tests.
-#     session.close()
-#     transaction.rollback()
-#     connection.close()
-
-
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="session")
 def client() -> Generator[TestClient, Any, None]:
     """
-    Create a new FastAPI TestClient that uses the `db_session` fixture to override
+    Create a new FastAPI TestClient that uses the `sqlalchemy_testing_session` fixture to override
     the `sqlalchemy_session` dependency that is injected into routes.
     """
+    # 1. Create docker container with postgres
+    testdb = create_postgres_in_docker()
 
-    SQLALCHEMY_DATABASE_URL = "postgresql://localhost:5434"
+    # # # MUST SLEEP # # #
+    # or engine will create before container is started and give `ConnectionRefusedError`
+    # This is because the container has to detach or it holds up the script
+    time.sleep(2)
 
-    engine = create_engine(
-        SQLALCHEMY_DATABASE_URL, echo=False, future=True
-    )
-    # Use connect_args parameter only with sqlite
+    # 2. Create engine and session
+    engine = create_engine("postgresql://postgres:postgres@localhost:5434", echo=False, future=True)
     SessionTesting = sessionmaker(bind=engine, autocommit=False, autoflush=False, future=True)
 
-    schemas = ['apartments', 'habits', 'moving', 'portfolio', 'priorities', 'tasks', 'tracks']
-    with SessionTesting() as session:
-        session.execute(text("CREATE DATABASE euphoria"))
-        for schema_name in schemas:  # SQLAlchemy will not create the schemas
-            session.execute(text(f"CREATE SCHEMA IF NOT EXISTS {schema_name}"))
-    Base.metadata.create_all(engine)  # Create the tables.
-
-    app = FastAPI()
-    app.include_router(tasks.router)
-
-    def _get_test_db() -> Session:
+    def sqlalchemy_testing_session() -> Session:
+        """Generate testing session for dependency override"""
         session = SessionTesting()
         try:
             yield session
         finally:
             session.close()
 
-    app.dependency_overrides[sqlalchemy_session] = _get_test_db
-    with TestClient(app) as client:
-        yield client
-    Base.metadata.drop_all(engine)
+    # 3. Create the schemas and tables
+    # Note: SQLAlchemy will not create the schemas automatically
+    schemas = ['apartments', 'habits', 'moving', 'portfolio', 'priorities', 'tasks', 'tracks']
+    with SessionTesting() as session:
+        for schema_name in schemas:
+            session.execute(CreateSchema(schema_name))
+        session.commit()
+
+    Base.metadata.create_all(engine)
+
+    # Create test data
+    # TODO: Find how to do this per testing file
+    def test_tasks(num: int = 1000) -> list[dict]:
+        return [
+            {
+                "name": f"task-{num:03}",
+                "category": f"category-{num:03}",
+                "priority": random.randint(1, 100),
+            }
+            for num in range(num)
+        ]
+
+    # Insert the test data
+    with SessionTesting() as session:
+        for task in test_tasks():
+            session.add(Task(**task))
+        session.commit()
+
+    # 4. Create the app
+    app = create_app_with_routes()
+
+    # 5. Replace regular session with testing session
+    app.dependency_overrides[sqlalchemy_session] = sqlalchemy_testing_session
+
+    # 6. Use TestClient to mimic requests
+    try:
+        with TestClient(app) as client:
+            yield client
+    finally:
+        # 6. Always delete the tables and stop the container when finished
+        Base.metadata.drop_all(engine)
+        testdb.stop()
--- euphoria/tests/endpoints/test_tasks.py
+++ euphoria/tests/endpoints/test_tasks.py
@@ -1,95 +1,95 @@
-import os
-
-os.environ['ENVIRONMENT'] = 'development'
-
-import requests
+import random
 import pytest
-import sqlite3
-from backend.api.main import app
-from backend.common.config import get_config_for_environment
-import json
 
+from backend.common.db.sqlalchemy.base_class import Base
+from backend.common.models.tasks import Task
 
-config = get_config_for_environment('development')
+SEED = 777
 ENDPOINT = '/tasks/'
-# ENDPOINT_URL = config.API_URL + ENDPOINT
 ENDPOINT_URL = 'http://localhost:8000' + ENDPOINT
-TEST_TASK_IDS = (1, 10, 50, 100, 250, 500, 1000)
+FAKE_TASKS_TO_CREATE = 1000
+NUM_TEST_TASKS = 5
+TEST_TASK_IDS = random.choices(list(range(1, FAKE_TASKS_TO_CREATE + 1)), k=NUM_TEST_TASKS)
 
 
-def create_tasks_test_database(num: int = 10):
+@pytest.fixture(scope="session", autouse=True)
+def test_tasks(num: int = 1000) -> list[dict]:
     return [
-        {"name": f"task-{num}", "category": f"category-{num}", "priority": num}
+        {
+            "name": f"task-{num:03}",
+            "category": f"category-{num:03}",
+            "priority": random.randint(1, 100),
+        }
         for num in range(num)
     ]
 
 
-TASKS_TO_CREATE = create_tasks_test_database()
+# @pytest.fixture(autouse=True)
+# def disable_network_calls(monkeypatch):
+#     def disabled():
+#         raise RuntimeError("Network access not allowed during testing!")
 
-
-@pytest.fixture(autouse=True)
-def disable_network_calls(monkeypatch):
-    def disabled():
-        raise RuntimeError("Network access not allowed during testing!")
-
-    monkeypatch.setattr(requests, "get", lambda *args, **kwargs: disabled())
-    monkeypatch.setattr(requests, "post", lambda *args, **kwargs: disabled())
-    monkeypatch.setattr(requests, "put", lambda *args, **kwargs: disabled())
-    monkeypatch.setattr(requests, "delete", lambda *args, **kwargs: disabled())
+#     monkeypatch.setattr(requests, "get", lambda *args, **kwargs: disabled())
+#     monkeypatch.setattr(requests, "post", lambda *args, **kwargs: disabled())
+#     monkeypatch.setattr(requests, "put", lambda *args, **kwargs: disabled())
+#     monkeypatch.setattr(requests, "delete", lambda *args, **kwargs: disabled())
 
 
 # @pytest.fixture(autouse=True)
 # def mock_environment_for_config(monkeypatch):
 #     monkeypatch.setenv('ENVIRONMENT', 'development')
 
+
 # ----- CRUD ----- #
 
 
-def test_read_one_task(client):
-    response = client.get(ENDPOINT + '830')
+@pytest.mark.parametrize('task_id', list(TEST_TASK_IDS))
+def test_read_one_task(task_id, client):
+    response = client.get(ENDPOINT + str(task_id))
     assert response.status_code == 200
 
 
 def test_read_many_tasks(client):
     response = client.get(ENDPOINT)
     assert response.status_code == 200
-    # assert len(response.json()) == 5
+    assert len(response.json()) == 1000
 
 
-def test_create_task(client):
-    print(TASKS_TO_CREATE[0])
-    response = client.post(ENDPOINT, data=json.dumps(TASKS_TO_CREATE[0]))
-    assert response.status_code == 200
+# def test_create_task(client):
+#     print(TASKS_TO_CREATE[0])
+#     response = client.post(ENDPOINT, data=json.dumps(TASKS_TO_CREATE[0]))
+#     assert response.status_code == 200
 
 
-def test_delete_task(client):
+@pytest.mark.parametrize('task_id', list(TEST_TASK_IDS))
+def test_delete_task(task_id, client):
     tasks = client.get(ENDPOINT).json()
-    task_to_delete = tasks[0]
-    response = client.delete(f'{ENDPOINT}{task_to_delete["id"]}/')
+    response = client.delete(f'{ENDPOINT}{task_id}/')
     assert response.status_code == 204
 
 
-# ----- EXTRA ENDPOINTS ----- #
+# # ----- EXTRA ENDPOINTS ----- #
 
 
-def test_complete_task(client):
-    response = client.post(ENDPOINT + 'complete/830/', data=TEST_TASK_IDS[0])
-    assert response.status_code == 200
+# @pytest.mark.parametrize('task_id', list(TEST_TASK_IDS))
+# def test_complete_task(task_id, client):
+#     response = client.post(ENDPOINT + f'complete/{task_id}/')
+#     assert response.status_code == 200
 
 
-def test_read_completed_tasks(client):
-    response = client.get(ENDPOINT + 'completed/')
-    print(ENDPOINT + 'completed/')
-    assert response.status_code == 200
+# def test_read_completed_tasks(client):
+#     response = client.get(ENDPOINT + 'completed/')
+#     print(ENDPOINT + 'completed/')
+#     assert response.status_code == 200
 
 
-def test_task_lifecycle(client):
-    original_task = TASKS_TO_CREATE[1]
-    created_task = client.post(ENDPOINT, data=json.dumps(original_task)).json()
-    assert created_task['name'] == original_task['name']
-    response_task = client.get(ENDPOINT + f'{created_task["id"]}').json()
-    assert response_task['name'] == original_task['name']
-    deleted_task = client.delete(ENDPOINT + f'{created_task["id"]}/').json()
-    assert deleted_task['name'] == original_task['name']
-    missing_task = client.get(ENDPOINT + f'{created_task["id"]}/')
-    assert missing_task.status_code == 404
+# def test_task_lifecycle(client):
+#     original_task = TASKS_TO_CREATE[1]
+#     created_task = client.post(ENDPOINT, data=json.dumps(original_task)).json()
+#     assert created_task['name'] == original_task['name']
+#     response_task = client.get(ENDPOINT + f'{created_task["id"]}').json()
+#     assert response_task['name'] == original_task['name']
+#     deleted_task = client.delete(ENDPOINT + f'{created_task["id"]}/').json()
+#     assert deleted_task['name'] == original_task['name']
+#     missing_task = client.get(ENDPOINT + f'{created_task["id"]}/')
+#     assert missing_task.status_code == 404
