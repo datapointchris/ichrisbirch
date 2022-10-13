8f250ce4273d04daa4c52ecf93925b0db2174fc7
Chris Birch <datapointchris@gmail.com>
2022-10-13 01:39:53 -0500

    ops: Change port for running dev scripts

--- euphoria/runappdev.py
+++ euphoria/runappdev.py
@@ -6,4 +6,4 @@ os.environ['ENVIRONMENT'] = os.environ['FLASK_DEBUG'] = 'development'
 if __name__ == '__main__':
     from backend.app.main import create_app
     app = create_app()
-    app.run(host='0.0.0.0', port=4000)
+    app.run(host='0.0.0.0', port=6000, debug=True)
