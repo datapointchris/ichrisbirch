1ebde25f494d8c1eb3bc7fd5c3655c8f1f44a9fc
Chris Birch <datapointchris@gmail.com>
2022-10-21 02:16:53 -0500

    release: Not going well...

--- prepare-version.sh
+++ prepare-version.sh
@@ -21,11 +21,12 @@
 # ./prepare-version.sh 0.5.2 "Add API Endpoint"
 ################################################################################
 
-SEMVER=$1
 # SEMVER is format: 1.1.1 -> useful for string replacement in files
-VERSION="v$1"
 # VERISON is format: v1.1.1 -> useful for folder and file names
+SEMVER=$1
+VERSION="v$1"
 VERSION_DESCRIPTION=$2
+
 echo "Preparing Release for version $SEMVER"
 
 # Make version directory
@@ -33,15 +34,16 @@ mkdir -p euphoria/stats/$VERSION
 echo "Version Stats Directory Created"
 
 # Update the version in pyproject.toml
-sed -r "s/version = \"[0-9]+\.[0-9]+\.[0-9]+\"/version = \"$SEMVER\"/" pyproject.toml
-echo "Version updated in `pyproject.toml`"
+sed -ri "s/version = \"[0-9]+\.[0-9]+\.[0-9]+\"/version = \"$SEMVER\"/" pyproject.toml
+echo "Version updated in pyproject.toml"
 
 # Update the version in euphoria/__init__.py
 echo "__version__ = '$SEMVER'" > euphoria/__init__.py
 echo "Updated version in euphoria/__init__.py"
 
 # Pytest and Coverage Report
-cd ../
+cd euphoria/
+pwd
 pytest --cov
 coverage report -m > stats/$VERSION/coverage.txt
 coverage json -o stats/$VERSION/coverage.json
@@ -49,12 +51,13 @@ echo "Created Pytest Coverage Report"
 
 # Lines of Code Files
 cd ../
-tokei . --exclude .venv --exclude euphoria/backend/alembic/versions/ > euphoria/version_stats/$VERSION/lines_of_code.txt
-tokei . --exclude .venv --exclude euphoria/backend/alembic/versions/ -o json > euphoria/version_stats/$VERSION/lines_of_code.json
+pwd
+tokei . --exclude .venv --exclude euphoria/backend/alembic/versions/ > euphoria/stats/$VERSION/lines_of_code.txt
+tokei . --exclude .venv --exclude euphoria/backend/alembic/versions/ -o json > euphoria/stats/$VERSION/lines_of_code.json
 echo "Created Lines of Code Files"
 
 # Wily Code Complexity
 # wily does not have json output at the moment
 wily build .
-wily diff . -r master > euphoria/version_stats/$VERSION/complexity.txt
+wily diff . -r master > euphoria/stats/$VERSION/complexity.txt
 echo "Created Code Complexity Report"
