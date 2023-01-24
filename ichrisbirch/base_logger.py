3a3437d940834d3aab9c8e91e39172a2170e3e24
Chris Birch <datapointchris@gmail.com>
2023-01-23 22:37:16 -0600

    bugfix: Fix log path

--- ichrisbirch/base_logger.py
+++ ichrisbirch/base_logger.py
@@ -3,20 +3,21 @@ import logging
 from ichrisbirch import settings
 
 logger = logging.getLogger(__name__)
+log_file = f'{settings.logging.LOG_PATH}/pylogger.log'
 
 formatter = logging.Formatter(settings.logging.LOG_FORMAT)
 
 console_log = logging.StreamHandler()
 console_log.setFormatter(formatter)
 
-file_log = logging.FileHandler(filename=settings.logging.LOG_PATH)
+file_log = logging.FileHandler(filename=log_file)
 file_log.setFormatter(formatter)
 
 logger.addHandler(console_log)
 logger.addHandler(file_log)
 
 logger.setLevel(settings.logging.LOG_LEVEL)
-logger.debug(f'Log Location: {settings.logging.LOG_PATH}')
+logger.debug(f'Log Location: {log_file}')
 
 # quiet matplotlib noisy output when in debug mode
 logging.getLogger('matplotlib').setLevel(logging.INFO)
