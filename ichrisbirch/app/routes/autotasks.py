dd63d0162efe9042b9542c05d39b2e7230bb3ee1
Chris Birch <datapointchris@gmail.com>
2023-04-28 00:44:18 -0500

    feat: Create autotasks route

---
+++ ichrisbirch/app/routes/autotasks.py
@@ -0,0 +1,58 @@
+import logging
+from typing import Any
+
+import requests
+from flask import Blueprint, abort, redirect, render_template, request, url_for
+
+from ichrisbirch import schemas
+from ichrisbirch.config import settings
+from ichrisbirch.models.autotask import TaskFrequency
+
+blueprint = Blueprint(
+    'autotasks',
+    __name__,
+    template_folder='templates/autotasks',
+    static_folder='static',
+)
+
+logger = logging.getLogger(__name__)
+
+AUTOTASKS_URL = f'{settings.API_URL}/autotasks'
+TASK_FREQUENCIES = [t.value for t in TaskFrequency]
+TIMEOUT = settings.REQUEST_TIMEOUT
+
+
+@blueprint.route('/', methods=['GET'])
+def index():
+    """Autotasks home endpoint"""
+    return render_template(
+        'autotasks/index.html',
+        autotasks=sorted(
+            [schemas.AutoTask(**task) for task in requests.get(AUTOTASKS_URL, timeout=TIMEOUT).json()],
+            key=lambda x: x.last_run_date,
+            reverse=True,
+        ),
+        task_frequencies=TASK_FREQUENCIES,
+    )
+
+
+@blueprint.route('/crud/', methods=['POST'])
+def crud():
+    """CRUD endpoint for autotasks"""
+    data: dict[str, Any] = request.form.to_dict()
+    method = data.pop('method')
+    logger.debug(f'{request.referrer=}')
+    logger.debug(f'{method=}')
+    logger.debug(f'{data}')
+    match method:
+        case 'add':
+            autotask = schemas.TaskCreate(**data).json()
+            response = requests.post(AUTOTASKS_URL, data=autotask, timeout=TIMEOUT)
+            logger.debug(response.text)
+            return redirect(url_for('autotasks.index'))
+        case 'delete':
+            autotask_id = data.get('id')
+            response = requests.delete(f'{AUTOTASKS_URL}/{autotask_id}', timeout=TIMEOUT)
+            logger.debug(response.text)
+            return redirect(url_for('autotasks.index'))
+    return abort(405, description=f"Method {method} not accepted")
