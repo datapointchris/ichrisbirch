1e197a389621172a08f2643ad5848c666be0efb8
Chris Birch <datapointchris@gmail.com>
2023-05-14 14:37:13 -0500

    feat: Generate and alembic revision for countdown table

---
+++ ichrisbirch/alembic/versions/cb0d2b146cda_add_countdown_table_to_the_database.py
@@ -0,0 +1,28 @@
+"""Add countdown table to the database
+
+Revision ID: cb0d2b146cda
+Revises: 733fbc2709b9
+Create Date: 2023-05-14 14:33:41.682515
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+# revision identifiers, used by Alembic.
+revision = 'cb0d2b146cda'
+down_revision = '733fbc2709b9'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column('countdowns', sa.Column('notes', sa.Text(), nullable=True))
+    op.add_column('countdowns', sa.Column('due_date', sa.DateTime(timezone=True), nullable=False))
+    op.drop_column('countdowns', 'date')
+
+
+def downgrade() -> None:
+    op.add_column('countdowns', sa.Column('date', postgresql.TIMESTAMP(), autoincrement=False, nullable=False))
+    op.drop_column('countdowns', 'due_date')
+    op.drop_column('countdowns', 'notes')
