39cb38e94ec19b4b443df06d40171c94b1b2f354
Chris Birch <datapointchris@gmail.com>
2023-05-14 16:32:27 -0500

    feat: Use settings title and add log_path to health endpoint

--- ichrisbirch/api/endpoints/health.py
+++ ichrisbirch/api/endpoints/health.py
@@ -19,11 +19,12 @@ def health() -> dict:
     Root Get
     """
     return {
-        'name': 'iChrisBirch API',
+        'name': settings.fastapi.title,
         'version': settings.version,
         'environment': os.getenv('ENVIRONMENT', 'NOT SET'),
         'api_url': os.getenv('API_URL', 'NOT SET'),
         'log_level': os.getenv('LOG_LEVEL', 'NOT SET'),
+        'log_path': settings.logging.log_path,
         'server_time': datetime.now().isoformat(),
         'local_time': datetime.now(tz=ZoneInfo('America/Chicago')).isoformat(),
     }
