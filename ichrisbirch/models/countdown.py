ae0f9c6e1eeb655cd735241c9106954fec99d193
Chris Birch <datapointchris@gmail.com>
2023-05-14 14:32:02 -0500

    feat: Add countdown model

--- ichrisbirch/models/countdown.py
+++ ichrisbirch/models/countdown.py
@@ -1,5 +1,7 @@
-from sqlalchemy import Column, DateTime, Integer, String
+from datetime import datetime
 
+from sqlalchemy import DateTime, Integer, String, Text
+from sqlalchemy.orm import Mapped, mapped_column
 from ichrisbirch.db.sqlalchemy.base import Base
 
 
@@ -7,9 +9,11 @@ class Countdown(Base):
     """SQLAlchemy model for countdowns table"""
 
     __tablename__ = 'countdowns'
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    date = Column(DateTime, nullable=False)
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(64), nullable=False)
+    notes: Mapped[str] = mapped_column(Text(), nullable=True)
+    due_date: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
 
     def __repr__(self):
-        return f'Countdown(name={self.name}, date={self.date}'
+        return f'Countdown(name={self.name}, notes={self.notes}, due_date={self.due_date}'
