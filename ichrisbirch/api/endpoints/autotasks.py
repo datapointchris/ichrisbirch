a025a11d04a7ef917cd7fd6bfa86b0446f6e7cea
Chris Birch <datapointchris@gmail.com>
2023-05-15 19:26:48 -0500

    refactor: Delete api crud and remove extra layer

--- ichrisbirch/api/endpoints/autotasks.py
+++ ichrisbirch/api/endpoints/autotasks.py
@@ -1,5 +1,6 @@
-from fastapi import Depends
-from fastapi_crudrouter import SQLAlchemyCRUDRouter
+import logging
+
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
@@ -8,23 +9,46 @@ from ichrisbirch.config import get_settings
 from ichrisbirch.db.sqlalchemy.session import sqlalchemy_session
 
 settings = get_settings()
-
-router = SQLAlchemyCRUDRouter(
-    prefix='/autotasks',
-    tags=['autotasks'],
-    db_model=models.AutoTask,
-    db=sqlalchemy_session,
-    schema=schemas.AutoTask,
-    create_schema=schemas.AutoTaskCreate,
-    update_route=False,
-    delete_all_route=False,
-    responses=settings.fastapi.responses,
-)
+router = APIRouter(prefix='/autotasks', tags=['autotasks'], responses=settings.fastapi.responses)
+logger = logging.getLogger(__name__)
 
 
-@router.get("/", response_model=list[schemas.AutoTask])
+@router.get('/', response_model=list[schemas.AutoTask])
 async def read_many(session: Session = Depends(sqlalchemy_session)):
     """API method to read many autotasks."""
-
     query = select(models.AutoTask).order_by(models.AutoTask.last_run_date.desc())
     return list(session.scalars(query).all())
+
+
+@router.post('/', response_model=schemas.AutoTask, status_code=status.HTTP_201_CREATED)
+async def create(task: schemas.AutoTaskCreate, session: Session = Depends(sqlalchemy_session)):
+    """API method to create a new task.  Passes request to crud.tasks module"""
+    db_obj = models.AutoTask(**task.dict())
+    session.add(db_obj)
+    session.commit()
+    session.refresh(db_obj)
+    return db_obj
+
+
+@router.get('/{id}/', response_model=schemas.AutoTask)
+async def read_one(id: int, session: Session = Depends(sqlalchemy_session)):
+    """API method to read one task.  Passes request to crud.tasks module"""
+    if task := session.get(models.AutoTask, id):
+        return task
+    else:
+        message = f'AutoTask {id} not found'
+        logger.warning(message)
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=message)
+
+
+@router.delete('/{id}/', status_code=200)
+async def delete(id: int, session: Session = Depends(sqlalchemy_session)):
+    """API method to delete a task.  Passes request to crud.tasks module"""
+    if task := session.get(models.AutoTask, id):
+        session.delete(task)
+        session.commit()
+        return task
+    else:
+        message = f'AutoTask {id} not found'
+        logger.warning(message)
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=message)
--- ichrisbirch/api/endpoints/tasks.py
+++ ichrisbirch/api/endpoints/tasks.py
@@ -1,82 +1,130 @@
+import logging
+from datetime import datetime
 from typing import Optional, Union
+from zoneinfo import ZoneInfo
 
 from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import or_, select
 from sqlalchemy.orm import Session
 
 # from ..dependencies import auth
-from ichrisbirch import schemas
-from ichrisbirch.api import crud
+from ichrisbirch import models, schemas
 from ichrisbirch.config import get_settings
 from ichrisbirch.db.sqlalchemy.session import sqlalchemy_session
 
+logger = logging.getLogger(__name__)
 settings = get_settings()
 router = APIRouter(prefix='/tasks', tags=['tasks'], responses=settings.fastapi.responses)
 
 
 @router.get("/", response_model=list[schemas.Task])
 async def read_many(
-    db: Session = Depends(sqlalchemy_session), completed_filter: Optional[str] = None, limit: Optional[int] = None
+    session: Session = Depends(sqlalchemy_session), completed_filter: Optional[str] = None, limit: Optional[int] = None
 ):
     """API method to read many tasks.  Passes request to crud.tasks module"""
-    print(f'{completed_filter=}')
-    return crud.tasks.read_many(db, completed_filter=completed_filter, limit=limit)
+    logger.debug(f'{completed_filter=}')
+
+    query = select(models.Task)
+    if completed_filter == 'completed':
+        query = query.filter(models.Task.complete_date.is_not(None))
+
+    if completed_filter == 'not_completed':
+        query = query.filter(models.Task.complete_date.is_(None))
+
+    query = query.order_by(models.Task.priority.asc(), models.Task.add_date.asc())
+    if limit:
+        query = query.limit(limit)
+    return list(session.scalars(query).all())
 
 
 @router.get("/completed/", response_model=list[schemas.TaskCompleted])
 async def completed(
-    db: Session = Depends(sqlalchemy_session),
+    session: Session = Depends(sqlalchemy_session),
     start_date: Union[str, None] = None,
     end_date: Union[str, None] = None,
     first: Union[bool, None] = None,
     last: Union[bool, None] = None,
 ):
     """API method to get completed tasks.  Passes request to crud.tasks module"""
-    return crud.tasks.completed(
-        db,
-        start_date=start_date,
-        end_date=end_date,
-        first=first,
-        last=last,
-    )
+    query = select(models.Task).filter(models.Task.complete_date.is_not(None))
+
+    if first:  # first completed task
+        query = query.order_by(models.Task.complete_date.asc()).limit(1)
+
+    elif last:  # most recent (last) completed task
+        query = query.order_by(models.Task.complete_date.desc()).limit(1)
+
+    elif start_date is None or end_date is None:  # return all if no start or end date
+        query = query.order_by(models.Task.complete_date.desc())
+
+    else:  # filtered by start and end date
+        query = query.filter(models.Task.complete_date >= start_date, models.Task.complete_date <= end_date).order_by(
+            models.Task.complete_date.desc()
+        )
+
+    return list(session.scalars(query).all())
 
 
-@router.post("/", response_model=schemas.Task)
-async def create(task: schemas.TaskCreate, db: Session = Depends(sqlalchemy_session)):
+@router.post('/', response_model=schemas.Task, status_code=201)
+async def create(task: schemas.TaskCreate, session: Session = Depends(sqlalchemy_session)):
     """API method to create a new task.  Passes request to crud.tasks module"""
-    return crud.tasks.create(task, db)
+    db_obj = models.Task(**task.dict())
+    session.add(db_obj)
+    session.commit()
+    session.refresh(db_obj)
+    return db_obj
 
 
-@router.get("/{task_id}/", response_model=schemas.Task)
-async def read_one(task_id: int, db: Session = Depends(sqlalchemy_session)):
+@router.get('/{task_id}/', response_model=schemas.Task)
+async def read_one(task_id: int, session: Session = Depends(sqlalchemy_session)):
     """API method to read one task.  Passes request to crud.tasks module"""
-    if not (task := crud.tasks.read_one(task_id, db)):
-        raise HTTPException(status_code=404, detail=f"Task {task_id} not found")
-    return task
+    if task := session.get(models.Task, task_id):
+        return task
+    else:
+        message = f'Task {task_id} not found'
+        logger.warning(message)
+        raise HTTPException(status_code=404, detail=message)
 
 
-# Not using, keep for reference
-# @router.post("/{task_id}/", response_model=schemas.Task)
-# async def update(task: schemas.Task, db: Session = Depends(sqlalchemy_session)):
-#     return crud.tasks.update(db, obj_in=task)
-
-
-@router.delete("/{task_id}/", status_code=200)
-async def delete(task_id: int, db: Session = Depends(sqlalchemy_session)):
+@router.delete('/{task_id}/', status_code=200)
+async def delete(task_id: int, session: Session = Depends(sqlalchemy_session)):
     """API method to delete a task.  Passes request to crud.tasks module"""
-    if not (task := crud.tasks.delete(task_id, db)):
-        raise HTTPException(status_code=404, detail=f"Task {task_id} not found")
-    return task
+    if task := session.get(models.Task, task_id):
+        session.delete(task)
+        session.commit()
+        return task
+    else:
+        message = f'Task {task_id} not found'
+        logger.warning(message)
+        raise HTTPException(status_code=404, detail=message)
 
 
-@router.post("/complete/{task_id}/", response_model=schemas.Task)
-async def complete(task_id: int, db: Session = Depends(sqlalchemy_session)):
+@router.post('/complete/{task_id}/', response_model=schemas.Task)
+async def complete(task_id: int, session: Session = Depends(sqlalchemy_session)):
     """API method to complete a task.  Passes request to crud.tasks module"""
-    if not (task := crud.tasks.complete_task(task_id, db)):
-        raise HTTPException(status_code=404, detail=f"Task {task_id} not found")
-    return task
+    if task := session.get(models.Task, task_id):
+        task.complete_date = datetime.now(tz=ZoneInfo("America/Chicago")).isoformat()  # type: ignore
+        session.add(task)
+        session.commit()
+        session.refresh(task)
+        return task
+    else:
+        message = f'Task {task_id} not found'
+        logger.warning(message)
+        raise HTTPException(status_code=404, detail=message)
 
 
-@router.get("/search/{search_terms}/", response_model=list[schemas.Task])
-async def search(search_terms: str, db: Session = Depends(sqlalchemy_session)):
+@router.get('/search/{search_terms}/', response_model=list[schemas.Task])
+async def search(search_terms: str, session: Session = Depends(sqlalchemy_session)):
     """API method to search for tasks"""
-    return crud.tasks.search(search_terms, db)
+    query = (
+        select(models.Task)
+        .filter(
+            or_(
+                models.Task.name.match(search_terms),
+                models.Task.notes.match(search_terms),
+            )
+        )
+        .order_by(models.Task.complete_date.desc(), models.Task.add_date.asc())
+    )
+    return list(session.scalars(query).all())
