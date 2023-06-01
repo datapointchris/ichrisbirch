53f4969dbe96b860d24cb461a264c796cf446b15
Chris Birch <datapointchris@gmail.com>
2023-05-31 23:53:57 -0500

    feat: Create test helper to display response description and content

---
+++ tests/helpers.py
@@ -0,0 +1,17 @@
+from fastapi import status
+from requests import Response, JSONDecodeError
+
+
+def show_status_and_response(response: Response) -> dict:
+    """Convert status code to description and return response if any"""
+    d = {}
+    for attr in dir(status):
+        code = attr.split('_')[1]
+        d[int(code)] = attr
+
+    try:
+        content = response.json()
+    except JSONDecodeError:
+        content = '<no response content>'
+
+    return {d.get(response.status_code, 'UNKNOWN'): content}
