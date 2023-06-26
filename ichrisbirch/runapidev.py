49cd10ec703d2b72f47e13c46ab85ffb11cc6d6c
Chris Birch <datapointchris@gmail.com>
2023-06-25 23:23:01 -0500

    feat: Remove runapp and runapi files, call using uvicorn and gunicorn directly

--- ichrisbirch/runapidev.py
+++
@@ -1,7 +0,0 @@
-import uvicorn
-
-from ichrisbirch.logger import create_base_logger
-
-if __name__ == "__main__":
-    logger = create_base_logger()
-    uvicorn.run('ichrisbirch.wsgi:api', port=6201, reload=True, log_level="debug")
--- ichrisbirch/runappdev.py
+++
@@ -1,6 +0,0 @@
-from ichrisbirch.logger import create_base_logger
-from ichrisbirch.wsgi import app
-
-if __name__ == '__main__':
-    logger = create_base_logger()
-    app.run(host='127.0.0.1', port=6200, debug=True)
