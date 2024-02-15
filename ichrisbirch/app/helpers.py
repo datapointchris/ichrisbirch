3f15e0a3cdc14a677cf799eaf34a6fa78cf845e5
Chris Birch <datapointchris@gmail.com>
2024-02-14 19:43:06 -0600

    feat: create url_builder for easier url concat

--- ichrisbirch/app/helpers.py
+++ ichrisbirch/app/helpers.py
@@ -1,19 +1,29 @@
 import logging
 
-import requests
+import httpx
 from flask import abort, flash
 
 
-def handle_if_not_response_code(response_code: int, response: requests.Response, logger: logging.Logger):
+def url_builder(base_url: str, *parts) -> str:
+    """Build a URL from a base URL and parts, will always include the trailing slash"""
+    stripped_parts = []
+    for part in parts:
+        if isinstance(part, (list, tuple, set)):
+            stripped_parts.extend([str(p).strip('/') for p in part])
+        elif isinstance(part, str):
+            stripped_parts.append(part.strip('/'))
+        elif isinstance(part, int):
+            stripped_parts.append(str(part))
+    return '/'.join([base_url.rstrip('/')] + stripped_parts) + '/'
+
+
+def handle_if_not_response_code(response_code: int, response: httpx.Response, logger: logging.Logger):
     '''Flash and log an error if the response status code is not the expected response code.
 
     Logger needs to be passed in as a parameter, or all logging is logged from the helpers file
     '''
     if response.status_code != response_code:
-        error_message = f'{response.url} : {response.status_code} {response.reason}'
+        error_message = f'expected {response_code} from {response.url} but received {response.status_code}'
         logger.error(error_message)
-        logger.error(response.text)
         flash(error_message, 'error')
-        flash(response.text, 'error')
-        abort_message = f'Expected {response_code} but received {response.status_code}, check logs for details'
-        abort(502, {'abort_message': abort_message, 'error_message': error_message})
+        abort(502, error_message)
---
+++ tests/app/test_helpers.py
@@ -0,0 +1,67 @@
+from ichrisbirch.app.helpers import url_builder
+
+BASE = 'http://url.com/autotasks/'
+
+
+def test_url_builder_single_string_with_leading_slash():
+    assert url_builder(BASE, '/one') == 'http://url.com/autotasks/one/'
+
+
+def test_url_builder_single_string_without_slash():
+    assert url_builder(BASE, 'two') == 'http://url.com/autotasks/two/'
+
+
+def test_url_builder_single_string_with_trailing_slash():
+    assert url_builder(BASE, '/three/') == 'http://url.com/autotasks/three/'
+
+
+def test_url_builder_single_string_with_slashes():
+    assert url_builder(BASE, '//five') == 'http://url.com/autotasks/five/'
+
+
+def test_url_builder_single_string_with_trailing_slashes():
+    assert url_builder(BASE, 'six//') == 'http://url.com/autotasks/six/'
+
+
+def test_url_builder_single_string_with_internal_slash():
+    assert url_builder(BASE, 'seven/eight') == 'http://url.com/autotasks/seven/eight/'
+
+
+def test_url_builder_single_string_with_multiple_internal_slashes():
+    assert url_builder(BASE, 'nine/ten/') == 'http://url.com/autotasks/nine/ten/'
+
+
+def test_url_builder_list_of_strings():
+    assert url_builder(BASE, ['test', 'fifteen', 'sixteen']) == 'http://url.com/autotasks/test/fifteen/sixteen/'
+
+
+def test_url_builder_list_of_strings_with_slashes():
+    assert url_builder(BASE, ['/test/', '/ssss//', 'eight/']) == 'http://url.com/autotasks/test/ssss/eight/'
+
+
+def test_url_builder_multiple_strings_with_slashes():
+    assert url_builder(BASE, '/not/', '/in//', 'collection/') == 'http://url.com/autotasks/not/in/collection/'
+
+
+def test_url_builder_single_integer():
+    assert url_builder(BASE, 1) == 'http://url.com/autotasks/1/'
+
+
+def test_url_builder_large_single_integer():
+    assert url_builder(BASE, 458) == 'http://url.com/autotasks/458/'
+
+
+def test_url_builder_multiple_integers():
+    assert url_builder(BASE, 1, 2, 3) == 'http://url.com/autotasks/1/2/3/'
+
+
+def test_url_builder_list_of_integers():
+    assert url_builder(BASE, [4, 5, 6]) == 'http://url.com/autotasks/4/5/6/'
+
+
+def test_url_builder_list_of_mixed_types():
+    assert url_builder(BASE, ['four/', 5, '//six/']) == 'http://url.com/autotasks/four/5/six/'
+
+
+def test_url_builder_mixed_types_with_slashes():
+    assert url_builder(BASE, 4, '//five//', 6, '/seven') == 'http://url.com/autotasks/4/five/6/seven/'
