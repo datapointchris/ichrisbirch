f1a3c426b599e0942594173d361b35dc2ae09301
Chris Birch <datapointchris@gmail.com>
2024-03-08 16:47:05 -0500

    feat: add box packing route, html, and style into app

--- ichrisbirch/app/routes/box_packing.py
+++ ichrisbirch/app/routes/box_packing.py
@@ -1,12 +1,25 @@
-import httpx
-from flask import Blueprint, redirect, render_template, request, url_for
+import logging
+from typing import Any
 
+import httpx
+import pydantic
+from fastapi import status
+from flask import Blueprint, Response, flash, redirect, render_template, request, url_for
+
+from ichrisbirch import schemas
+from ichrisbirch.app.helpers import handle_if_not_response_code, url_builder
 from ichrisbirch.config import get_settings
-from ichrisbirch.database.sqlalchemy import session
-from ichrisbirch.models.box_packing import Box, Item
+from ichrisbirch.models.box import BoxSize
 
 settings = get_settings()
 blueprint = Blueprint('box_packing', __name__, template_folder='templates/box_packing', static_folder='static')
+
+logger = logging.getLogger(__name__)
+
+BOX_PACKING_API_URL = f'{settings.api_url}/box_packing/'
+BOXES_API_URL = url_builder(BOX_PACKING_API_URL, 'boxes')
+ITEMS_API_URL = url_builder(BOX_PACKING_API_URL, 'items')
+BOX_SIZES = [s.value for s in BoxSize]
 
 
 @blueprint.route('/', methods=['GET', 'POST'])
@@ -14,70 +27,100 @@ def index():
     """Box packing homepage"""
     last_box_id = request.args.get('last_box_id')
     box_id = last_box_id or request.form.get('selected_box_id')
-    with session:
-        boxes = session.query(Box).all()
-        print('SELECTED BOX')
-        print(session.get(Box, box_id))
-        if selected_box := session.get(Box, box_id):
-            print(selected_box)
-            box_items = selected_box.items
-            print(box_items)
-        else:
-            box_items = None
-
-    return render_template(
-        'box_packing/index.html',
-        boxes=boxes,
-        selected_box=selected_box,
-        box_items=box_items,
-    )
+    response = httpx.get(url_builder(BOXES_API_URL))
+    handle_if_not_response_code(200, response, logger)
+    boxes = [schemas.Box(**box) for box in response.json()]
+    logger.debug(f'{box_id=}')
+    selected_box = next((box for box in boxes if str(box.id) == box_id), None)
+    logger.debug(f'{selected_box=}')
+    return render_template('box_packing/index.html', selected_box=selected_box, boxes=boxes, box_sizes=BOX_SIZES)
 
 
 @blueprint.route('/all/', methods=['GET', 'POST'])
 def all():
     """All packed boxes"""
-    sort_1 = request.form.get('sort_1', '')
-    sort_2 = request.form.get('sort_2', '')
-    with session:
-        boxes = session.query(Box)
-        if sort_1:
-            if not sort_2:
-                boxes = boxes.order_by(getattr(Box, sort_1)).all()
-            else:
-                boxes = boxes.order_by(getattr(Box, sort_1), getattr(Box, sort_2)).all()
-    return render_template('box_packing/all.html', boxes=boxes)
+    sort_1 = request.form.get('sort_1')
+    sort_2 = request.form.get('sort_2')
+    logger.debug(f'{sort_1=} {sort_2=}')
+
+    params = {'sort_1': sort_1, 'sort_2': sort_2}
+    response = httpx.get(BOXES_API_URL, params=params)
+    handle_if_not_response_code(200, response, logger)
+    boxes = [schemas.Box(**box) for box in response.json()]
+    logger.debug(f'{boxes=}')
+    # TODO: check if sort_1 will always be present
+    if sort_1:
+        if sort_2:
+            boxes = sorted(boxes, key=lambda box: (getattr(box, sort_1), getattr(box, sort_2)))
+        else:
+            boxes = sorted(boxes, key=lambda box: getattr(box, sort_1))
+    return render_template('box_packing/all.html', boxes=boxes, sort_1=sort_1, sort_2=sort_2)
 
 
 @blueprint.route('/search/', methods=['GET', 'POST'])
 def search():
-    """Search for a packed box"""
-    search_text = request.form.get('search_text')
-    results = session.query(Item).filter(Item.name.match(search_text)).all()
-    return render_template(
-        'box_packing/search.html',
-        results=results,
-    )
+    if request.method == 'POST':
+        data: dict[str, Any] = request.form.to_dict()
+        search_text = data.get('search_text')
+        logger.debug(f'{request.referrer=} | {search_text=}')
+        if not search_text:
+            flash('No search terms provided', 'warning')
+            return render_template('box_packing/search.html', results=[])
+        search_url = url_builder(BOX_PACKING_API_URL, 'search')
+        response = httpx.get(search_url, params={'q': search_text})
+        logger.debug(f'{response.json()=}')
+        handle_if_not_response_code(200, response, logger)
+        results = [schemas.BoxItem(**item) for item in response.json()]
+    else:
+        results = []
+    return render_template('box_packing/search.html', results=results)
 
 
-# TODO: [2023/02/10] - Make this crud instead of form
-@blueprint.route('/form/', methods=['POST'])
-def form():
-    """CRUD operations for boxes"""
-    api_url = settings.api_url
-    data = request.form.to_dict()
+@blueprint.route('/crud/', methods=['POST'])
+def crud():
+    data: dict[str, Any] = request.form.to_dict()
     method = data.pop('method')
-    match method:
-        case ['add_box']:
-            box = Box(**data)
-            httpx.post(f'{api_url}/boxes', data=box)
-        case ['delete_box']:
-            box = Box(**data)
-            httpx.delete(f'{api_url}/boxes/{box.id}')
-        case ['add_item']:
-            item = Item(**data)
-            httpx.post(f'{api_url}/boxes/items', data=box)
-        case ['delete_item']:
-            item = Item(**data)
-            httpx.delete(f'{api_url}/boxes/items/{item.id}')
+    logger.debug(f'{request.referrer=} {method=}')
+    logger.debug(f'{data=}')
 
-    return redirect(url_for('box_packing.index'))
+    if method == 'add_box':
+        try:
+            essential = bool(data.pop('essential', 0))
+            warm = bool(data.pop('warm', 0))
+            liquid = bool(data.pop('liquid', 0))
+            box = schemas.BoxCreate(**data, essential=essential, warm=warm, liquid=liquid)
+        except pydantic.ValidationError as e:
+            logger.exception(e)
+            flash(str(e), 'error')
+            return redirect(request.referrer or url_for('box_packing.index'))
+        response = httpx.post(BOXES_API_URL, content=box.model_dump_json())
+        handle_if_not_response_code(201, response, logger)
+        return redirect(request.referrer or url_for('box_packing.index'))
+
+    if method == 'delete_box':
+        url = url_builder(BOXES_API_URL, data.get('id'))
+        response = httpx.delete(url)
+        handle_if_not_response_code(204, response, logger)
+        return redirect(request.referrer or url_for('box_packing.index'))
+
+    if method == 'add_item':
+        try:
+            essential = bool(data.pop('essential', 0))
+            warm = bool(data.pop('warm', 0))
+            liquid = bool(data.pop('liquid', 0))
+            item = schemas.BoxItemCreate(**data, essential=essential, warm=warm, liquid=liquid)
+        except pydantic.ValidationError as e:
+            logger.error(e)
+            flash(str(e), 'error')
+            return redirect(request.referrer or url_for('box_packing.index'))
+        response = httpx.post(ITEMS_API_URL, content=item.model_dump_json())
+        handle_if_not_response_code(201, response, logger)
+        return redirect(request.referrer or url_for('box_packing.index'))
+
+    if method == 'delete_item':
+        url = url_builder(ITEMS_API_URL, data.get('id'))
+        response = httpx.delete(url)
+        handle_if_not_response_code(204, response, logger)
+        return redirect(request.referrer or url_for('box_packing.index'))
+
+    return Response(f'Method {method} not accepted', status=status.HTTP_405_METHOD_NOT_ALLOWED)
