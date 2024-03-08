4bafd7f22c6f09aaebb5b1f615beb9503df80c64
Chris Birch <datapointchris@gmail.com>
2024-03-08 17:12:36 -0500

    feat: convert box size from str to BoxSize enum into postgres box_packing.boxes table

---
+++ ichrisbirch/alembic/versions/6d0ab5ffd636_create_box_size_enum.py
@@ -0,0 +1,25 @@
+"""create box size enum
+
+Revision ID: 6d0ab5ffd636
+Revises: 74af91565f9a
+Create Date: 2024-02-24 21:14:01.681253
+
+"""
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = '6d0ab5ffd636'
+down_revision = '74af91565f9a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.execute("CREATE TYPE boxsize as ENUM('BOOK', 'SMALL', 'MEDIUM', 'LARGE', 'BAG', 'MONITOR', 'MISC')")
+    op.execute("ALTER TABLE box_packing.boxes ALTER COLUMN SIZE TYPE boxsize USING SIZE::boxsize")
+
+
+def downgrade() -> None:
+    op.execute("ALTER TABLE box_packing.boxes ALTER COLUMN SIZE TYPE VARCHAR USING SIZE::VARCHAR")
+    op.execute("DROP TYPE boxsize")
