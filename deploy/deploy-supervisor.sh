48c708ea258b0d06eac4bbdddd9c9c9593ae4931
Chris Birch <datapointchris@gmail.com>
2024-03-11 01:57:22 -0400

    feat: update supervisor deploy script

--- deploy/deploy-supervisor.sh
+++ deploy/deploy-supervisor.sh
@@ -1,198 +1,106 @@
 #!/usr/bin/env bash
 
-######################################################################
-# Deploy to Environment
-######################################################################
+# Deploy Supervisor Config Files to [environment]
 
 # Fail script on command fail
 set -e
 
+PROJECT_NAME=$(basename "$(git rev-parse --show-toplevel)")
 
-SCRIPT_NAME="$0"
-
-usage() {
-    local message
-    message=$(cat <<-EOF
-        $(green "$(print_section "Deploy Supervisor and NGINX Config Files")")
-
-        $(green "Usage:")
-        Run as $SCRIPT_NAME {dev, test, prod}
-
-        $(green "Example:")
-        $SCRIPT_NAME dev
-
-        $(green "Help:")
-        $SCRIPT_NAME --help
-EOF
-)
-    echo "$message"
-}
-
-
-help() {
-    local message
-    message=$(cat <<-EOF
-        $(usage)
-
-        $(green "Script Actions:")
-        Copy: NGINX config file
-        Copy: NGINX sites-available config file(s)
-        Symlink: NGINX sites-enabled config file(s)
-
-        Copy: Supervisor config file
-        Copy: Supervisor api config file
-        Copy: Supervisor app config file
-        Create: Supervisor api log files
-        Create: Supervisor app log files
-
-        $(green "Dependencies")
-        nginx
-        supervisor
-EOF
-)
-    echo "$message"
-}
-
-
-#-------------------- SCRIPT PRE-PROCESSING --------------------#
-# Get project name from current dir
-PROJECT=$(basename "$PWD")
-
-# -- Check for arguments -- #
-# NOTE: No exit codes inside the functions so they can be more generic
-
-# 1. If no arguments: show usage, no error
-if [[ -z "$1" ]]; then
-    usage
-    exit 0;
-
-# 2. Check for particular flags
-elif [[ "$1" = "--help" ]]; then
-    shift
-    help "$@"
-    exit 0;
+if [[ $(uname) == "Darwin" ]]; then
+    MACOS=1
+    OS_PREFIX="/usr/local"
+    ENVIRONMENT="dev"
+else
+    OS_PREFIX=""
+    ENVIRONMENT="prod"
 fi
 
-# 3. Assign variables
-# Get environment flag
-ENVIRONMENT=$1
-case $ENVIRONMENT in
-    dev) OS_PREFIX="/usr/local" ;;
-    test) OS_PREFIX="" ;;
-    prod) OS_PREFIX="" ;;
-    *) echo_error "Unsupported environment flag: $ENVIRONMENT"
-       usage
-       exit 1 ;;
-esac
-
-#-------------------- MAIN SCRIPT --------------------#
-
 # Project File Locations
-ENV_DEPLOY_DIR="deploy/$ENVIRONMENT"
-NGINX_DEPLOY_DIR="$ENV_DEPLOY_DIR/nginx"
-SUPERVISOR_DEPLOY_DIR="$ENV_DEPLOY_DIR/supervisor"
+SUPERVISOR_SOURCE="$ENVIRONMENT/supervisor"
 
 # Config Locations
 ETC_DIR="$OS_PREFIX/etc"
 
-NGINX_DIR="$ETC_DIR/nginx"
-NGINX_SITES_AVAILABLE="$NGINX_DIR/sites-available"
-NGINX_SITES_ENABLED="$NGINX_DIR/sites-enabled"
+SUPERVISOR_HOME="$ETC_DIR/supervisor"
+SUPERVISOR_CONFIG_TARGET="$SUPERVISOR_HOME/conf.d"
+SUPERVISOR_LOG_TARGET="$OS_PREFIX/var/log/supervisor"
 
-SUPERVISOR_DIR="$ETC_DIR/supervisor"
-SUPERVISOR_CONFIG_DIR="$SUPERVISOR_DIR/conf.d"
-SUPERVISOR_LOG_DIR="$OS_PREFIX/var/log/supervisor"
+dry_run() {
+    if [[ $MACOS ]]; then
+        echo "MacOS detected: /usr/local prefix will be used"
+    fi
+    echo Script Actions:
+    {
+    echo "copy: ./$SUPERVISOR_SOURCE/supervisord.conf => $ETC_DIR/supervisord.conf"
+    echo "copy: ./$SUPERVISOR_SOURCE/api.conf => $SUPERVISOR_CONFIG_TARGET/$PROJECT_NAME-api.conf"
+    echo "copy: ./$SUPERVISOR_SOURCE/app.conf => $SUPERVISOR_CONFIG_TARGET/$PROJECT_NAME-app.conf"
+    echo "copy: ./$SUPERVISOR_SOURCE/scheduler.conf => $SUPERVISOR_CONFIG_TARGET/$PROJECT_NAME-scheduler.conf"
+    } | column -t
+    echo "create: $SUPERVISOR_LOG_TARGET/supervisord.log"
+    echo "create: $SUPERVISOR_LOG_TARGET/$PROJECT_NAME-api.log"
+    echo "create: $SUPERVISOR_LOG_TARGET/$PROJECT_NAME-app.log"
+    echo "create: $SUPERVISOR_LOG_TARGET/$PROJECT_NAME-scheduler.log"
+}
 
+# -- Check for arguments -- #
+# NOTE: No exit codes inside usage or help so they can be called without exiting
 
-print_title "Deploying $PROJECT project in $ENVIRONMENT environment"
+if [[ "$1" = "--dry-run" ]]; then
+    dry_run
+    exit 0;
+fi
 
-
-# ---------- NGINX ----------- #
-print_section "NGINX"
-
-# Directories
-sudo mkdir -p $NGINX_SITES_AVAILABLE
-sudo mkdir -p $NGINX_SITES_ENABLED
-
-notify_create $NGINX_SITES_AVAILABLE
-notify_create $NGINX_SITES_ENABLED
-
-# Configuration
-sudo cp "$NGINX_DEPLOY_DIR/nginx.conf" "$NGINX_DIR/nginx.conf"
-sudo cp "$NGINX_DEPLOY_DIR/api.conf" "$NGINX_SITES_AVAILABLE/$PROJECT-api.conf"
-sudo cp "$NGINX_DEPLOY_DIR/app.conf" "$NGINX_SITES_AVAILABLE/$PROJECT-app.conf"
-
-notify_copy "$NGINX_DEPLOY_DIR/nginx.conf" "$NGINX_DIR/nginx.conf"
-notify_copy "$NGINX_DEPLOY_DIR/api.conf" "$NGINX_SITES_AVAILABLE/$PROJECT-api.conf"
-notify_copy "$NGINX_DEPLOY_DIR/app.conf" "$NGINX_SITES_AVAILABLE/$PROJECT-app.conf"
-
-# Symlink from sites-available to sites-enabled
-sudo ln -sf "$NGINX_SITES_AVAILABLE/$PROJECT-api.conf" "$NGINX_SITES_ENABLED/$PROJECT-api.conf"
-sudo ln -sf "$NGINX_SITES_AVAILABLE/$PROJECT-app.conf" "$NGINX_SITES_ENABLED/$PROJECT-app.conf"
-
-notify_symlink "$NGINX_SITES_AVAILABLE/$PROJECT-api.conf" "$NGINX_SITES_ENABLED/$PROJECT-api.conf"
-notify_symlink "$NGINX_SITES_AVAILABLE/$PROJECT-app.conf" "$NGINX_SITES_ENABLED/$PROJECT-app.conf"
-echo
-
-
-# ---------- Supervisor ---------- #
-print_section "SUPERVISOR"
+echo "Deploying Supervisor Config Files to $ENVIRONMENT"
 # Note: Make sure log direcitories match entries in `supervisord.conf`
 
-# Directories
-sudo mkdir -p $SUPERVISOR_DIR
-sudo mkdir -p $SUPERVISOR_CONFIG_DIR
-sudo mkdir -p $SUPERVISOR_LOG_DIR
+sudo mkdir -vp $SUPERVISOR_HOME
+sudo mkdir -vp $SUPERVISOR_CONFIG_TARGET
+sudo mkdir -vp $SUPERVISOR_LOG_TARGET
 
-notify_create $SUPERVISOR_DIR
-notify_create $SUPERVISOR_CONFIG_DIR
-notify_create $SUPERVISOR_LOG_DIR
+if [[ ! -f "$ETC_DIR/supervisord.conf" ]] || ! diff -q "$SUPERVISOR_SOURCE/supervisord.conf" "$ETC_DIR/supervisord.conf" &>/dev/null; then
+    sudo cp -v "$SUPERVISOR_SOURCE/supervisord.conf" "$ETC_DIR/supervisord.conf"
+    SUPERVISORD_UPDATED=1
+fi
 
-# Configs
-sudo cp "$SUPERVISOR_DEPLOY_DIR/supervisord.conf" "$SUPERVISOR_DIR/supervisord.conf"
-sudo cp "$SUPERVISOR_DEPLOY_DIR/app.conf" "$SUPERVISOR_CONFIG_DIR/$PROJECT-app.conf"
-sudo cp "$SUPERVISOR_DEPLOY_DIR"/api.conf "$SUPERVISOR_CONFIG_DIR/$PROJECT-api.conf"
+CONFIG_FILES=("app.conf" "api.conf" "scheduler.conf")
+for CONFIG_FILE in "${CONFIG_FILES[@]}"; do
+    SOURCE="$SUPERVISOR_SOURCE/$CONFIG_FILE"
+    TARGET="$SUPERVISOR_CONFIG_TARGET/$PROJECT_NAME-$CONFIG_FILE"
+    if [[ ! -f "$TARGET" ]] || ! diff -q "$SOURCE" "$TARGET" &>/dev/null; then
+        sudo cp -v "$SOURCE" "$TARGET"
+    fi
+done
+echo
 
-notify_copy "$SUPERVISOR_DEPLOY_DIR/supervisord.conf" "$SUPERVISOR_DIR/supervisord.conf"
-notify_copy "$SUPERVISOR_DEPLOY_DIR/app.conf" "$SUPERVISOR_CONFIG_DIR/$PROJECT-app.conf"
-notify_copy "$SUPERVISOR_DEPLOY_DIR/api.conf" "$SUPERVISOR_CONFIG_DIR/$PROJECT-api.conf"
+LOG_FILES=("supervisord.log" "$PROJECT_NAME-app.log" "$PROJECT_NAME-api.log" "$PROJECT_NAME-scheduler.log")
+for LOG_FILE in "${LOG_FILES[@]}"; do
+    TARGET="$SUPERVISOR_LOG_TARGET/$LOG_FILE"
+    if [[ -f "$TARGET" ]]; then
+        echo "log file exists: $TARGET"
+    else
+        sudo touch "$TARGET"
+        echo created log file: "$TARGET"
+    fi
+done
+echo
 
-# Logs
-sudo touch "$SUPERVISOR_LOG_DIR/supervisord.log"
-sudo touch "$SUPERVISOR_LOG_DIR/$PROJECT-app.log"
-sudo touch "$SUPERVISOR_LOG_DIR/$PROJECT-api.log"
-
-notify_create "$SUPERVISOR_LOG_DIR/supervisord.log"
-notify_create "$SUPERVISOR_LOG_DIR/$PROJECT-app.log"
-notify_create "$SUPERVISOR_LOG_DIR/$PROJECT-api.log"
-echo ""
-
-
-# ---------- Set owner on MacOS in dev ---------- #
-if [[ $ENVIRONMENT = "dev" ]]; then
-    print_section "PERMISSIONS"
-    sudo chown -R "$USER" $NGINX_DIR
-    sudo chown -R "$USER" $SUPERVISOR_DIR
-    sudo chown -R "$USER" $SUPERVISOR_LOG_DIR
-    echo "Change owner of $(green "$NGINX_DIR/") to user: $(blue "$USER")"
-    echo "Change owner of $(green "$SUPERVISOR_DIR/") to user: $(blue "$USER")"
-    echo "Change owner of $(green "$SUPERVISOR_LOG_DIR/") to user: $(blue "$USER")"
+# Set owner on MacOS in dev
+if [[ $MACOS ]]; then
+    echo "Updating MacOS owner permissions to: $USER"
+    sudo chown -vR "$USER" $SUPERVISOR_HOME
+    sudo chown -vR "$USER" $SUPERVISOR_LOG_TARGET
     echo
 fi
 
+if [[ $SUPERVISORD_UPDATED ]]; then
+    echo restarting supervisor
+    sudo supervisorctl reload
+else
+    echo restarting supervisor processes
+    sudo supervisorctl reread
+    sudo supervisorctl update
+fi
 
-# ---------- Apache ---------- #
-# Change permissions for www folder
-# sudo chown -R $USER:www-data /var/www
-# sudo find /var/www -type d -exec chmod 2750 {} \+
-# sudo find /var/www -type f -exec chmod 640 {} \+
-
-
-
-# ---------- Success ---------- #
-print_title "All files copied"
-echo
-blue "Remember to restart nginx and supervisor:"
-echo "sudo supervisorctl reload"
-echo "sudo nginx -s reload"
-echo
+echo Supervisor Status:
+sudo supervisorctl status
