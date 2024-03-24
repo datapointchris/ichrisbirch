48d9076e1ff1d5f6861ddc7fefb124c4ab22d868
Chris Birch <datapointchris@gmail.com>
2024-03-24 11:42:50 -0400

    test(app): add test for server_stats route

---
+++ tests/app/routes/test_server_stats.py
@@ -0,0 +1,8 @@
+from fastapi import status
+
+from tests.helpers import show_status_and_response
+
+
+def test_get_server_stats(test_app):
+    response = test_app.get('/server_stats/')
+    assert response.status_code == status.HTTP_200_OK, show_status_and_response(response)
