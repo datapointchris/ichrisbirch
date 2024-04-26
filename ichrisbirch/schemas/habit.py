70937964067eae34394e5b87f6915cf98a2ce092
Chris Birch <datapointchris@gmail.com>
2024-04-26 15:18:31 -0400

    feat(habits): create habit pydantic schema

---
+++ ichrisbirch/schemas/habit.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+from pydantic import BaseModel, ConfigDict
+
+
+class HabitConfig(BaseModel):
+    model_config = ConfigDict(from_attributes=True, use_enum_values=True)
+
+
+class HabitCreate(HabitConfig):
+    name: str
+    category_id: int
+    is_current: bool
+
+
+class Habit(HabitConfig):
+    id: int
+    name: str
+    category_id: int
+    is_current: bool
+
+
+class HabitUpdate(HabitConfig):
+    name: str | None = None
+    category_id: int | None = None
+    is_current: bool | None = None
+
+
+class HabitCompleted(HabitConfig):
+    id: int
+    name: str
+    category_id: int
+    complete_date: datetime
