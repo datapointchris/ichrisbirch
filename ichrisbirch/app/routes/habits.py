184cbc22c309c6760a8ee9c95a5e87d911d815c6
Chris Birch <datapointchris@gmail.com>
2024-04-27 00:38:01 -0400

    feat(app): update habits home endpoint

--- ichrisbirch/app/routes/habits.py
+++ ichrisbirch/app/routes/habits.py
@@ -1,119 +1,127 @@
 import logging
-from datetime import date
 
 import httpx
-from flask import Blueprint, redirect, render_template, request, url_for
+from flask import Blueprint, render_template
 
-from ichrisbirch.app.easy_dates import EasyDate
+from ichrisbirch import schemas
+from ichrisbirch.app.easy_dates import EasyDateTime
+from ichrisbirch.app.helpers import handle_if_not_response_code, url_builder
 from ichrisbirch.config import get_settings
-from ichrisbirch.database.sqlalchemy import session
-from ichrisbirch.models.habit import Category, CompletedHabit, Habit
 
 settings = get_settings()
 logger = logging.getLogger(__name__)
 blueprint = Blueprint('habits', __name__, template_folder='templates/habits', static_folder='static')
 
 
-HABITS_URL = f'{settings.api_url}/habits'
+HABITS_API_URL = f'{settings.api_url}/habits/'
 
 
 @blueprint.route('/', methods=['GET', 'POST'])
 def index():
-    """Habits homepage"""
-    with session:
-        # current_habits = session.query(Habit).where(Habit.current.is_(True))
-        completed_today = session.query(CompletedHabit).where(CompletedHabit.completed_date == date.today())
-        not_completed = session.query(Habit).join()
 
-    return render_template('index.html', completed_today=completed_today, not_completed=not_completed)
+    edt = EasyDateTime()
+    start_date, end_date = edt.filters.get('today', (None, None))
+    params = {'start_date': str(start_date), 'end_date': str(end_date)}
 
+    completed_response = httpx.get(url_builder(HABITS_API_URL, 'completed'), params=params)
+    handle_if_not_response_code(200, completed_response, logger)
+    completed = [schemas.Habit(**habit) for habit in completed_response.json()]
 
-@blueprint.route('/completed/', methods=['GET', 'POST'])
-def completed():
-    """Completed habits"""
-    if request.method == 'POST':
-        selected_habit = request.form.get('habit')
-        date_filter = request.form.get('filter')
-    else:
-        selected_habit = None
-        date_filter = 'this_week'
+    daily_response = httpx.get(HABITS_API_URL)
+    handle_if_not_response_code(200, daily_response, logger)
+    daily = [schemas.Habit(**habit) for habit in daily_response.json()]
 
-    with session:
-        q = session.query(CompletedHabit)
-        q = q.join(Habit, CompletedHabit.habit_id == Habit.id)
-        if selected_habit:
-            q = q.where(Habit.name == selected_habit)
-        first_completed = q.order_by(CompletedHabit.completed_date.asc()).first()
-        last_completed = q.order_by(CompletedHabit.completed_date.desc()).first()
+    not_completed = [habit for habit in daily if habit not in completed]
 
-        habits = session.query(Habit).all()
-
-    ed = EasyDate()
-    filters = {
-        'today': (ed.today, ed.tomorrow),
-        'yesterday': (ed.yesterday, ed.today),
-        'this_week': (ed.week_start, ed.week_end),
-        'last_7': (ed.previous_7, ed.today),
-        'this_month': (ed.this_month, ed.next_month),
-        'last_30': (ed.previous_30, ed.today),
-        'this_year': (ed.this_year, ed.next_year),
-        'all': (first_completed.complete_date, last_completed.complete_date),
-    }
-
-    start_date, end_date = filters.get(date_filter)
-
-    with session:
-        q = session.query(CompletedHabit)
-        q = q.where(
-            CompletedHabit.completed_date >= start_date,
-            CompletedHabit.completed_date < end_date,
-        )
-        completed = q.order_by(CompletedHabit.completed_date.desc()).all()
-
-    return render_template(
-        'habits/completed.html',
-        habits=habits,
-        completed=completed,
-        filters=filters,
-        date_filter=date_filter,
-        long_date=ed.today.strftime('%A %B %d, %Y'),
-    )
+    return render_template('habits/index.html', completed_today=completed, not_completed=not_completed)
 
 
-@blueprint.route('/manage/', methods=['GET', 'POST'])
-def manage():
-    """Manage habits page"""
-    if request.method == 'POST':
-        data = request.form.to_dict()
-        action = data.pop('action')
-        habit = Habit(**data)
-        print(action, habit)
+# @blueprint.route('/completed/', methods=['GET', 'POST'])
+# def completed():
+#     """Completed habits"""
+#     if request.method == 'POST':
+#         selected_habit = request.form.get('habit')
+#         date_filter = request.form.get('filter')
+#     else:
+#         selected_habit = None
+#         date_filter = 'this_week'
 
-    return render_template('habits/manage.html')
+#     with session:
+#         q = session.query(CompletedHabit)
+#         q = q.join(Habit, CompletedHabit.habit_id == Habit.id)
+#         if selected_habit:
+#             q = q.where(Habit.name == selected_habit)
+#         first_completed = q.order_by(CompletedHabit.completed_date.asc()).first()
+#         last_completed = q.order_by(CompletedHabit.completed_date.desc()).first()
+
+#         habits = session.query(Habit).all()
+
+#     ed = EasyDate()
+#     filters = {
+#         'today': (ed.today, ed.tomorrow),
+#         'yesterday': (ed.yesterday, ed.today),
+#         'this_week': (ed.week_start, ed.week_end),
+#         'last_7': (ed.previous_7, ed.today),
+#         'this_month': (ed.this_month, ed.next_month),
+#         'last_30': (ed.previous_30, ed.today),
+#         'this_year': (ed.this_year, ed.next_year),
+#         'all': (first_completed.complete_date, last_completed.complete_date),
+#     }
+
+#     start_date, end_date = filters.get(date_filter)
+
+#     with session:
+#         q = session.query(CompletedHabit)
+#         q = q.where(
+#             CompletedHabit.completed_date >= start_date,
+#             CompletedHabit.completed_date < end_date,
+#         )
+#         completed = q.order_by(CompletedHabit.completed_date.desc()).all()
+
+#     return render_template(
+#         'habits/completed.html',
+#         habits=habits,
+#         completed=completed,
+#         filters=filters,
+#         date_filter=date_filter,
+#         long_date=ed.today.strftime('%A %B %d, %Y'),
+#     )
 
 
-# TODO: [2023/02/10] - change form to crud
-@blueprint.route('/form/', methods=['POST'])
-def form():
-    """CRUD operations for habits"""
-    api_url = settings.api_url
-    data = request.form.to_dict()
-    action = data.pop('action')
-    match action:
-        case ['add_habit']:
-            habit = Habit(**data)
-            httpx.post(f'{api_url}/habits', data=habit)
-        case ['delete_habit']:
-            habit = Habit(**data)
-            httpx.delete(f'{api_url}/habits/{habit.id}')
-        case ['complete_habit']:
-            habit = Habit(**data)
-            httpx.post(f'{api_url}/habits/completed', data=habit)
-        case ['add_category']:
-            category = Category(**data)
-            httpx.post(f'{api_url}/habits/categories', data=category)
-        case ['delete_category']:
-            category = Category(**data)
-            httpx.delete(f'{api_url}/habits/categories/{category.id}')
+# @blueprint.route('/manage/', methods=['GET', 'POST'])
+# def manage():
+#     """Manage habits page"""
+#     if request.method == 'POST':
+#         data = request.form.to_dict()
+#         action = data.pop('action')
+#         habit = Habit(**data)
+#         print(action, habit)
 
-    return redirect(url_for('habits.index'))
+#     return render_template('habits/manage.html')
+
+
+# # TODO: [2023/02/10] - change form to crud
+# @blueprint.route('/form/', methods=['POST'])
+# def form():
+#     """CRUD operations for habits"""
+#     api_url = settings.api_url
+#     data = request.form.to_dict()
+#     action = data.pop('action')
+#     match action:
+#         case ['add_habit']:
+#             habit = Habit(**data)
+#             httpx.post(f'{api_url}/habits', data=habit)
+#         case ['delete_habit']:
+#             habit = Habit(**data)
+#             httpx.delete(f'{api_url}/habits/{habit.id}')
+#         case ['complete_habit']:
+#             habit = Habit(**data)
+#             httpx.post(f'{api_url}/habits/completed', data=habit)
+#         case ['add_category']:
+#             category = Category(**data)
+#             httpx.post(f'{api_url}/habits/categories', data=category)
+#         case ['delete_category']:
+#             category = Category(**data)
+#             httpx.delete(f'{api_url}/habits/categories/{category.id}')
+
+#     return redirect(url_for('habits.index'))
--- ichrisbirch/models/__init__.py
+++ ichrisbirch/models/__init__.py
@@ -3,7 +3,7 @@ from ichrisbirch.models.box import Box  # noqa
 from ichrisbirch.models.boxitem import BoxItem  # noqa
 from ichrisbirch.models.countdown import Countdown  # noqa
 from ichrisbirch.models.event import Event  # noqa
-from ichrisbirch.models.habit import Habit  # noqa
+from ichrisbirch.models.habit import Habit, HabitCategory, HabitCompleted  # noqa
 from ichrisbirch.models.task import Task  # noqa
 
-__all__ = ['AutoTask', 'Box', 'BoxItem', 'Countdown', 'Event', 'Habit', 'Task']
+__all__ = ['AutoTask', 'Box', 'BoxItem', 'Countdown', 'Event', 'Habit', 'HabitCategory', 'HabitCompleted', 'Task']
