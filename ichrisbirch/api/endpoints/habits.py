ed1d3ffcdea93cfbd936693780b714f41c442984
Chris Birch <datapointchris@gmail.com>
2024-04-27 00:17:23 -0400

    fix(api): endpoints that have path parameters must be specified last in the file

--- ichrisbirch/api/endpoints/habits.py
+++ ichrisbirch/api/endpoints/habits.py
@@ -34,6 +34,66 @@ async def read_many_habits(
     return list(session.scalars(query).all())
 
 
+@router.post('/categories/', response_model=schemas.HabitCategory, status_code=status.HTTP_201_CREATED)
+async def create_category(category: schemas.HabitCategoryCreate, session: Session = Depends(sqlalchemy_session)):
+    db_obj = models.HabitCategory(**category.model_dump())
+    session.add(db_obj)
+    session.commit()
+    session.refresh(db_obj)
+    return db_obj
+
+
+@router.get("/categories/", response_model=list[schemas.HabitCategory], status_code=status.HTTP_200_OK)
+async def read_many_categories(
+    session: Session = Depends(sqlalchemy_session), current: Optional[bool] = None, limit: Optional[int] = None
+):
+    query = select(models.HabitCategory).limit(limit)
+    if current:
+        query = query.filter(models.HabitCategory.is_current.is_(True))
+    return list(session.scalars(query).all())
+
+
+@router.post('/completed/', response_model=schemas.HabitCompleted, status_code=status.HTTP_201_CREATED)
+async def create_completed(habit: schemas.HabitCompletedCreate, session: Session = Depends(sqlalchemy_session)):
+    db_obj = models.HabitCompleted(**habit.model_dump())
+    session.add(db_obj)
+    session.commit()
+    session.refresh(db_obj)
+    return db_obj
+
+
+@router.get("/completed/", response_model=list[schemas.HabitCompleted], status_code=status.HTTP_200_OK)
+async def read_many_completed(
+    session: Session = Depends(sqlalchemy_session),
+    start_date: Union[str, None] = None,
+    end_date: Union[str, None] = None,
+    first: Union[bool, None] = None,
+    last: Union[bool, None] = None,
+):
+    logger.debug(f'Parameters passed: {start_date=}, {end_date=}, {first=}, {last=}')
+    query = select(models.HabitCompleted)
+
+    if first:  # first completed
+        query = query.order_by(models.HabitCompleted.complete_date.asc()).limit(1)
+
+    elif last:  # most recent (last) completed
+        query = query.order_by(models.HabitCompleted.complete_date.desc()).limit(1)
+
+    elif start_date is None or end_date is None:  # return all if no start or end date
+        query = query.order_by(models.HabitCompleted.complete_date.desc())
+
+    else:  # filtered by start and end date
+        query = query.filter(
+            models.HabitCompleted.complete_date >= start_date, models.HabitCompleted.complete_date <= end_date
+        )
+        query = query.order_by(models.HabitCompleted.complete_date.desc())
+
+    return list(session.scalars(query).all())
+
+
+######################
+
+
 @router.get('/{habit_id}/', response_model=schemas.Habit, status_code=status.HTTP_200_OK)
 async def read_one_habit(habit_id: int, session: Session = Depends(sqlalchemy_session)):
     if habit := session.get(models.Habit, habit_id):
@@ -73,25 +133,6 @@ async def complete_habit(habit_id: int, session: Session = Depends(sqlalchemy_se
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=message)
 
 
-@router.post('/categories/', response_model=schemas.HabitCategory, status_code=status.HTTP_201_CREATED)
-async def create_category(category: schemas.HabitCategoryCreate, session: Session = Depends(sqlalchemy_session)):
-    db_obj = models.HabitCategory(**category.model_dump())
-    session.add(db_obj)
-    session.commit()
-    session.refresh(db_obj)
-    return db_obj
-
-
-@router.get("/categories/", response_model=list[schemas.HabitCategory], status_code=status.HTTP_200_OK)
-async def read_many_categories(
-    session: Session = Depends(sqlalchemy_session), current: Optional[bool] = None, limit: Optional[int] = None
-):
-    query = select(models.HabitCategory).limit(limit)
-    if current:
-        query = query.filter(models.HabitCategory.is_current.is_(True))
-    return list(session.scalars(query).all())
-
-
 @router.get('/categories/{category_id}/', response_model=schemas.HabitCategory, status_code=status.HTTP_200_OK)
 async def read_one_category(category_id: int, session: Session = Depends(sqlalchemy_session)):
     if habit := session.get(models.HabitCategory, category_id):
@@ -112,44 +153,6 @@ async def delete_category(category_id: int, session: Session = Depends(sqlalchem
         message = f'Habit Category {category_id} not found'
         logger.warning(message)
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=message)
-
-
-@router.post('/completed/', response_model=schemas.HabitCompleted, status_code=status.HTTP_201_CREATED)
-async def create_completed(habit: schemas.HabitCompletedCreate, session: Session = Depends(sqlalchemy_session)):
-    db_obj = models.HabitCompleted(**habit.model_dump())
-    session.add(db_obj)
-    session.commit()
-    session.refresh(db_obj)
-    return db_obj
-
-
-@router.get("/completed/", response_model=list[schemas.HabitCompleted], status_code=status.HTTP_200_OK)
-async def read_many_completed(
-    session: Session = Depends(sqlalchemy_session),
-    start_date: Union[str, None] = None,
-    end_date: Union[str, None] = None,
-    first: Union[bool, None] = None,
-    last: Union[bool, None] = None,
-):
-    logger.debug(f'Parameters passed: {start_date=}, {end_date=}, {first=}, {last=}')
-    query = select(models.HabitCompleted)
-
-    if first:  # first completed
-        query = query.order_by(models.HabitCompleted.complete_date.asc()).limit(1)
-
-    elif last:  # most recent (last) completed
-        query = query.order_by(models.HabitCompleted.complete_date.desc()).limit(1)
-
-    elif start_date is None or end_date is None:  # return all if no start or end date
-        query = query.order_by(models.HabitCompleted.complete_date.desc())
-
-    else:  # filtered by start and end date
-        query = query.filter(
-            models.HabitCompleted.complete_date >= start_date, models.HabitCompleted.complete_date <= end_date
-        )
-        query = query.order_by(models.HabitCompleted.complete_date.desc())
-
-    return list(session.scalars(query).all())
 
 
 @router.get('/completed/{completed_id}/', response_model=schemas.HabitCompleted, status_code=status.HTTP_200_OK)
