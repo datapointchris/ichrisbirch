79b114bb954804ae648d943e0012c14e02f41a09
Chris Birch <datapointchris@gmail.com>
2024-04-27 02:18:20 -0400

    feat(habits): create all frontend crud endpoints

--- ichrisbirch/app/routes/habits.py
+++ ichrisbirch/app/routes/habits.py
@@ -1,7 +1,12 @@
 import logging
+from collections import Counter
+from datetime import datetime, timedelta
+from typing import Any
 
 import httpx
-from flask import Blueprint, render_template
+import pydantic
+from fastapi import status
+from flask import Blueprint, Response, flash, redirect, render_template, request, url_for
 
 from ichrisbirch import schemas
 from ichrisbirch.app.easy_dates import EasyDateTime
@@ -9,11 +14,34 @@ from ichrisbirch.app.helpers import handle_if_not_response_code, url_builder
 from ichrisbirch.config import get_settings
 
 settings = get_settings()
-logger = logging.getLogger(__name__)
 blueprint = Blueprint('habits', __name__, template_folder='templates/habits', static_folder='static')
 
+logger = logging.getLogger(__name__)
 
-HABITS_API_URL = f'{settings.api_url}/habits/'
+HABITS_API_URL = url_builder(settings.api_url, 'habits')
+
+
+def create_completed_habit_chart_data(habits: list[schemas.HabitCompleted]) -> tuple[list[str], list[int]]:
+    """Create chart labels and values from completed habits for chart.js"""
+
+    # Ex: Friday, January 01, 2001
+    DATE_FORMAT = '%A, %B %d, %Y'
+
+    first = habits[0]
+    last = habits[-1]
+
+    all_timestamps_for_range = [
+        first.complete_date + timedelta(days=x) for x in range((last.complete_date - first.complete_date).days)
+    ]
+    timestamps_zero_counts = {timestamp: 0 for timestamp in all_timestamps_for_range}
+    completed_counts = Counter(
+        [datetime(habit.complete_date.year, habit.complete_date.month, habit.complete_date.day) for habit in habits]
+    )
+    all_dates_with_counts = timestamps_zero_counts | completed_counts
+    # Reversed for chronological display in chart.js
+    chart_labels = [datetime.strftime(dt, DATE_FORMAT) for dt in reversed(all_dates_with_counts)]
+    chart_values = list(reversed(list(all_dates_with_counts.values())))
+    return chart_labels, chart_values
 
 
 @blueprint.route('/', methods=['GET', 'POST'])
@@ -36,92 +64,105 @@ def index():
     return render_template('habits/index.html', completed_today=completed, not_completed=not_completed)
 
 
-# @blueprint.route('/completed/', methods=['GET', 'POST'])
-# def completed():
-#     """Completed habits"""
-#     if request.method == 'POST':
-#         selected_habit = request.form.get('habit')
-#         date_filter = request.form.get('filter')
-#     else:
-#         selected_habit = None
-#         date_filter = 'this_week'
+@blueprint.route('/completed/', methods=['GET', 'POST'])
+def completed():
+    """Completed habits"""
+    DEFAULT_DATE_FILTER = 'this_week'
+    edt = EasyDateTime()
+    selected_filter = request.form.get('filter', '') if request.method == 'POST' else DEFAULT_DATE_FILTER
+    start_date, end_date = edt.filters.get(selected_filter, (None, None))
+    logger.debug(f'date filter: {selected_filter} = {start_date} - {end_date}')
 
-#     with session:
-#         q = session.query(CompletedHabit)
-#         q = q.join(Habit, CompletedHabit.habit_id == Habit.id)
-#         if selected_habit:
-#             q = q.where(Habit.name == selected_habit)
-#         first_completed = q.order_by(CompletedHabit.completed_date.asc()).first()
-#         last_completed = q.order_by(CompletedHabit.completed_date.desc()).first()
+    if start_date is None or end_date is None:
+        params = {}
+    else:
+        params = {'start_date': str(start_date), 'end_date': str(end_date)}
 
-#         habits = session.query(Habit).all()
+    completed_response = httpx.get(url_builder(HABITS_API_URL, 'completed'), params=params)
+    handle_if_not_response_code(200, completed_response, logger)
+    completed_habits = [schemas.HabitCompleted(**habit) for habit in completed_response.json()]
+    if completed_habits:
+        completed_count = len(completed_habits)
+        chart_labels, chart_values = create_completed_habit_chart_data(completed_habits)
+    else:
+        completed_count = f"No completed habits for time period: {' '.join(selected_filter.split('_')).capitalize()}"
+        chart_labels, chart_values = None, None
 
-#     ed = EasyDate()
-#     filters = {
-#         'today': (ed.today, ed.tomorrow),
-#         'yesterday': (ed.yesterday, ed.today),
-#         'this_week': (ed.week_start, ed.week_end),
-#         'last_7': (ed.previous_7, ed.today),
-#         'this_month': (ed.this_month, ed.next_month),
-#         'last_30': (ed.previous_30, ed.today),
-#         'this_year': (ed.this_year, ed.next_year),
-#         'all': (first_completed.complete_date, last_completed.complete_date),
-#     }
+    daily_response = httpx.get(HABITS_API_URL)
+    handle_if_not_response_code(200, daily_response, logger)
+    daily = [schemas.Habit(**habit) for habit in daily_response.json()]
 
-#     start_date, end_date = filters.get(date_filter)
-
-#     with session:
-#         q = session.query(CompletedHabit)
-#         q = q.where(
-#             CompletedHabit.completed_date >= start_date,
-#             CompletedHabit.completed_date < end_date,
-#         )
-#         completed = q.order_by(CompletedHabit.completed_date.desc()).all()
-
-#     return render_template(
-#         'habits/completed.html',
-#         habits=habits,
-#         completed=completed,
-#         filters=filters,
-#         date_filter=date_filter,
-#         long_date=ed.today.strftime('%A %B %d, %Y'),
-#     )
+    return render_template(
+        'habits/completed.html',
+        habits=daily,
+        completed=completed_habits,
+        completed_count=completed_count,
+        filters=list(edt.filters) + ['all'],  # additional 'all' filter to frontend
+        date_filter=selected_filter,
+        chart_labels=chart_labels,
+        chart_values=chart_values,
+        zip=zip,
+        long_date=edt.today.strftime('%A %B %d, %Y'),
+    )
 
 
-# @blueprint.route('/manage/', methods=['GET', 'POST'])
-# def manage():
-#     """Manage habits page"""
-#     if request.method == 'POST':
-#         data = request.form.to_dict()
-#         action = data.pop('action')
-#         habit = Habit(**data)
-#         print(action, habit)
+@blueprint.route('/manage/', methods=['GET', 'POST'])
+def manage():
+    """Manage habits page"""
+    # if request.method == 'POST':
+    #     data = request.form.to_dict()
+    #     action = data.pop('action')
+    #     habit = Habit(**data)
+    #     print(action, habit)
 
-#     return render_template('habits/manage.html')
+    return render_template('habits/manage.html')
 
 
-# # TODO: [2023/02/10] - change form to crud
-# @blueprint.route('/form/', methods=['POST'])
-# def form():
-#     """CRUD operations for habits"""
-#     api_url = settings.api_url
-#     data = request.form.to_dict()
-#     action = data.pop('action')
-#     match action:
-#         case ['add_habit']:
-#             habit = Habit(**data)
-#             httpx.post(f'{api_url}/habits', data=habit)
-#         case ['delete_habit']:
-#             habit = Habit(**data)
-#             httpx.delete(f'{api_url}/habits/{habit.id}')
-#         case ['complete_habit']:
-#             habit = Habit(**data)
-#             httpx.post(f'{api_url}/habits/completed', data=habit)
-#         case ['add_category']:
-#             category = Category(**data)
-#             httpx.post(f'{api_url}/habits/categories', data=category)
-#         case ['delete_category']:
-#             category = Category(**data)
-#             httpx.delete(f'{api_url}/habits/categories/{category.id}')
+@blueprint.route('/crud/', methods=['POST'])
+def crud():
+    data: dict[str, Any] = request.form.to_dict()
+    action = data.pop('action')
+    logger.debug(f'{request.referrer=} {action=}')
+    logger.debug(f'{data=}')
 
-#     return redirect(url_for('habits.index'))
+    if action == 'add_habit':
+        try:
+            category = schemas.HabitCreate(**data)
+        except pydantic.ValidationError as e:
+            logger.exception(e)
+            flash(str(e), 'error')
+            return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
+        response = httpx.post(HABITS_API_URL, content=category.model_dump_json())
+        handle_if_not_response_code(201, response, logger)
+        return redirect(request.referrer or url_for('habits.manage'))
+
+    elif action == 'complete_habit':
+        url = url_builder(HABITS_API_URL, 'complete', data.get('id'))
+        response = httpx.post(url)
+        handle_if_not_response_code(200, response, logger)
+        return redirect(request.referrer or url_for('habits.manage'))
+
+    elif action == 'delete_habit':
+        url = url_builder(HABITS_API_URL, data.get('id'))
+        response = httpx.delete(url)
+        handle_if_not_response_code(204, response, logger)
+        return redirect(request.referrer or url_for('habits.manage'))
+
+    elif action == 'add_category':
+        try:
+            category = schemas.HabitCategoryCreate(**data)
+        except pydantic.ValidationError as e:
+            logger.exception(e)
+            flash(str(e), 'error')
+            return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
+        response = httpx.post(url_builder(HABITS_API_URL, 'categories'), content=category.model_dump_json())
+        handle_if_not_response_code(201, response, logger)
+        return redirect(request.referrer or url_for('habits.manage'))
+
+    elif action == 'delete_category':
+        url = url_builder(HABITS_API_URL, 'categories', data.get('id'))
+        response = httpx.delete(url)
+        handle_if_not_response_code(204, response, logger)
+        return redirect(request.referrer or url_for('habits.manage'))
+
+    return Response(f'Method/Action {action} not accepted', status=status.HTTP_405_METHOD_NOT_ALLOWED)
