d07f0395e6e8615afb6d5a6da66ed21980c246d1
Chris Birch <datapointchris@gmail.com>
2024-04-26 23:15:29 -0400

    feat(api): add habits endpoints to root export

--- ichrisbirch/api/endpoints/__init__.py
+++ ichrisbirch/api/endpoints/__init__.py
@@ -3,9 +3,10 @@ from ichrisbirch.api.endpoints import (  # noqa: F401, E402
     box_packing,
     countdowns,
     events,
+    habits,
     home,
     server_stats,
     tasks,
 )
 
-__all__ = ['autotasks', 'box_packing', 'countdowns', 'events', 'server_stats', 'home', 'tasks']
+__all__ = ['autotasks', 'box_packing', 'countdowns', 'events', 'habits', 'home', 'server_stats', 'tasks']
