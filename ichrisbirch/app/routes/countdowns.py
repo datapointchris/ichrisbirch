1c5dccefaab80249b2175e05ef129bb91957e1c8
Chris Birch <datapointchris@gmail.com>
2024-05-02 00:30:32 -0400

    refactor(app/countdowns): convert countdowns route to use the new QueryAPI class

--- ichrisbirch/app/routes/countdowns.py
+++ ichrisbirch/app/routes/countdowns.py
@@ -1,21 +1,15 @@
 import logging
 from typing import Any
 
-import httpx
-import pydantic
 from fastapi import status
-from flask import Blueprint, Response, flash, redirect, render_template, request, url_for
+from flask import Blueprint, Response, render_template, request
 
 from ichrisbirch import schemas
-from ichrisbirch.app.helpers import handle_if_not_response_code, url_builder
-from ichrisbirch.config import get_settings
+from ichrisbirch.app.query_api import QueryAPI
 
-settings = get_settings()
 blueprint = Blueprint('countdowns', __name__, template_folder='templates/countdowns', static_folder='static')
-
 logger = logging.getLogger(__name__)
-
-COUNTDOWNS_API_URL = f'{settings.api_url}/countdowns/'
+countdowns_api = QueryAPI(base_url='countdowns', api_key='', logger=logger, response_model=schemas.Countdown)
 
 
 @blueprint.route('/', methods=['GET', 'POST'])
@@ -27,24 +21,13 @@ def index():
         logger.debug(f'{data=}')
 
         if action == 'add':
-            try:
-                countdown = schemas.CountdownCreate(**data).model_dump_json()
-            except pydantic.ValidationError as e:
-                logger.exception(e)
-                flash(str(e), 'error')
-                return redirect(request.referrer or url_for('countdowns.index'))
-            response = httpx.post(COUNTDOWNS_API_URL, content=countdown)
-            handle_if_not_response_code(201, response, logger)
+            countdowns_api.post(data=data)
 
         elif action == 'delete':
-            url = url_builder(COUNTDOWNS_API_URL, data.get('id'))
-            response = httpx.delete(url)
-            handle_if_not_response_code(204, response, logger)
+            countdowns_api.delete(data.get('id'))
 
         else:
             return Response(f'Method/Action {action} not allowed', status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
-    response = httpx.get(COUNTDOWNS_API_URL)
-    handle_if_not_response_code(200, response, logger)
-    countdowns = [schemas.Countdown(**countdown) for countdown in response.json()]
+    countdowns = countdowns_api.get()
     return render_template('countdowns/index.html', countdowns=countdowns)
