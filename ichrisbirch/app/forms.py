520eee13e5a83c3340dbafc4f5c78df2327b751b
Chris Birch <datapointchris@gmail.com>
2024-05-04 01:07:58 -0400

    feat(auth): create signup and login forms using wtforms

---
+++ ichrisbirch/app/forms.py
@@ -0,0 +1,29 @@
+from flask_wtf import FlaskForm
+from wtforms import BooleanField
+from wtforms import PasswordField
+from wtforms import StringField
+from wtforms import SubmitField
+from wtforms.validators import DataRequired
+from wtforms.validators import Email
+from wtforms.validators import EqualTo
+from wtforms.validators import Length
+
+
+class SignupForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[Length(min=6), Email(message='Enter a valid email.'), DataRequired()])
+    password = PasswordField(
+        'Password', validators=[DataRequired(), Length(min=6, message='Select a stronger password.')]
+    )
+    confirm = PasswordField(
+        'Confirm Your Password', validators=[DataRequired(), EqualTo('password', message='Passwords must match.')]
+    )
+    # website = StringField('Website', validators=[Optional()])
+    submit = SubmitField('Register')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email(message='Enter a valid email.')])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Log In')
