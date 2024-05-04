604e3f94abd69323d776688a000d8c55ef56f8dd
Chris Birch <datapointchris@gmail.com>
2024-05-04 00:31:54 -0400

    refactor(server): rename server_stats to server across project

--- tests/app/routes/test_serverstats.py
+++ tests/app/routes/test_serverstats.py
@@ -3,6 +3,6 @@ from fastapi import status
 from tests.helpers import show_status_and_response
 
 
-def test_get_server_stats(test_app):
-    response = test_app.get('/server-stats/')
+def test_get_serverstats(test_app):
+    response = test_app.get('/server/')
     assert response.status_code == status.HTTP_200_OK, show_status_and_response(response)
