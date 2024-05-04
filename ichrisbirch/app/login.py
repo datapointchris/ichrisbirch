1dc22e50ad01ef3124dafb22a7b49dd3c5b84fd8
Chris Birch <datapointchris@gmail.com>
2024-05-04 01:10:38 -0400

    feat(app/auth): add login manager for flask-login

---
+++ ichrisbirch/app/login.py
@@ -0,0 +1,34 @@
+import logging
+
+from flask import flash
+from flask import redirect
+from flask import url_for
+from flask_login import LoginManager
+
+from ichrisbirch import schemas
+from ichrisbirch.app.query_api import QueryAPI
+
+logger = logging.getLogger(__name__)
+
+login_manager = LoginManager()
+
+user_api = QueryAPI(base_url='users', api_key='', logger=logger, response_model=schemas.User)
+
+
+@login_manager.user_loader
+def load_user(alternative_id):
+    """
+    Reload the user object from the user ID stored in the session.
+    Must return the User object or None
+    Using the user ID as the value of the remember token means you must change
+    the user ID to invalidate their login sessions.
+    One way to improve this is to use an alternative user id instead of the user ID.
+    """
+    return user_api.get(['alt', alternative_id])
+
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    """Redirect unauthorized users to Login page."""
+    flash('You must be logged in to view that page.')
+    return redirect(url_for('auth.login'))
