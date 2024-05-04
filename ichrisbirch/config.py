45eafcff9803e348f5c500430c724f6379e2b3f7
Chris Birch <datapointchris@gmail.com>
2024-05-04 01:08:46 -0400

    feat(config): update config with flask-login settings

--- ichrisbirch/config.py
+++ ichrisbirch/config.py
@@ -2,11 +2,20 @@ import functools
 import logging
 import os
 import pathlib
+from datetime import timedelta
 from typing import Optional
 
 import dotenv
 
 logger = logging.getLogger(__name__)
+
+
+class FastAPISettings:
+    def __init__(self):
+        self.host: Optional[str] = os.getenv('FASTAPI_HOST')
+        self.port: Optional[str] = os.getenv('FASTAPI_PORT')
+        self.title: str = 'iChrisBirch API'
+        self.description: str = """## Backend API for iChrisBirch.com"""
 
 
 class FlaskSettings:
@@ -18,12 +27,42 @@ class FlaskSettings:
         self.DEBUG: Optional[bool] = bool(os.getenv('FLASK_DEBUG'))
 
 
-class FastAPISettings:
+class FlaskLoginSettings:
     def __init__(self):
-        self.host: Optional[str] = os.getenv('FASTAPI_HOST')
-        self.port: Optional[str] = os.getenv('FASTAPI_PORT')
-        self.title: str = 'iChrisBirch API'
-        self.description: str = """## Backend API for iChrisBirch.com"""
+        self.login_view: str = 'auth.login'
+        self.login_message: str = 'Please log in to access this page.'
+        self.login_message_category: str = 'info'
+        self.REMEMBER_COOKIE_DURATION: timedelta = timedelta(days=1)
+        self.REMEMBER_COOKIE_DOMAIN: str = 'ichrisbirch.com'
+        self.SESSION_PROTECTION: str = 'strong'
+
+
+class GithubSettings:
+    def __init__(self):
+        self.api_token: Optional[str] = os.getenv('GITHUB_API_TOKEN')
+        self.api_url_issues: str = 'https://api.github.com/repos/datapointchris/ichrisbirch/issues'
+        self.api_url_labels: str = 'https://api.github.com/repos/datapointchris/ichrisbirch/labels'
+        self.api_headers = {
+            'Accept': 'application/vnd.github+json',
+            'Authorization': f'Bearer {self.api_token}',
+            'X-GitHub-Api-Version': '2022-11-28',
+        }
+
+
+class MongoDBSettings:
+    def __init__(self):
+        self.host: Optional[str] = os.getenv('MONGO_HOST')
+        self.user: Optional[str] = os.getenv('MONGO_USER')
+        self.password: Optional[str] = os.getenv('MONGO_PASSWORD')
+
+    @property
+    def db_uri(self) -> str:
+        return f'mongodb://{self.user}:{self.password}@{self.host}'
+
+
+class PlaywrightSettings:
+    def __init__(self):
+        self.timeout = 5_000
 
 
 class PostgresSettings:
@@ -54,37 +93,9 @@ class SQLAlchemySettings:
         return f'postgresql://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}'
 
 
-class MongoDBSettings:
-    def __init__(self):
-        self.host: Optional[str] = os.getenv('MONGO_HOST')
-        self.user: Optional[str] = os.getenv('MONGO_USER')
-        self.password: Optional[str] = os.getenv('MONGO_PASSWORD')
-
-    @property
-    def db_uri(self) -> str:
-        return f'mongodb://{self.user}:{self.password}@{self.host}'
-
-
 class SQLiteSettings:
     def __init__(self):
         self.db_uri: Optional[str] = os.getenv('SQLITE_DATABASE_URI')
-
-
-class PlaywrightSettings:
-    def __init__(self):
-        self.timeout = 5_000
-
-
-class GithubSettings:
-    def __init__(self):
-        self.api_token: Optional[str] = os.getenv('GITHUB_API_TOKEN')
-        self.api_url_issues: str = 'https://api.github.com/repos/datapointchris/ichrisbirch/issues'
-        self.api_url_labels: str = 'https://api.github.com/repos/datapointchris/ichrisbirch/labels'
-        self.api_headers = {
-            'Accept': 'application/vnd.github+json',
-            'Authorization': f'Bearer {self.api_token}',
-            'X-GitHub-Api-Version': '2022-11-28',
-        }
 
 
 class Settings:
@@ -95,14 +106,15 @@ class Settings:
         self.ENV_FILE: pathlib.Path = env_file
         self.REQUEST_TIMEOUT: int = 3
 
-        self.flask = FlaskSettings()
         self.fastapi = FastAPISettings()
+        self.flask = FlaskSettings()
+        self.flasklogin = FlaskLoginSettings()
+        self.github = GithubSettings()
+        self.mongodb = MongoDBSettings()
+        self.playwright = PlaywrightSettings()
         self.postgres = PostgresSettings()
         self.sqlalchemy = SQLAlchemySettings()
-        self.mongodb = MongoDBSettings()
         self.sqlite = SQLiteSettings()
-        self.playwright = PlaywrightSettings()
-        self.github = GithubSettings()
 
     @property
     def api_url(self) -> str:
