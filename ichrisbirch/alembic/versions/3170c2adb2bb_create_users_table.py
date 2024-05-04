92b26c772ee7094d38cd335054d764a1bdab80b5
Chris Birch <datapointchris@gmail.com>
2024-05-04 02:58:12 -0400

    feat(db): alembic migration for adding users table

---
+++ ichrisbirch/alembic/versions/3170c2adb2bb_create_users_table.py
@@ -0,0 +1,38 @@
+"""Create users table
+
+Revision ID: 3170c2adb2bb
+Revises: 02bb8d55588c
+Create Date: 2024-05-04 01:54:51.507817
+
+"""
+
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy.dialects import postgresql
+
+# revision identifiers, used by Alembic.
+revision = '3170c2adb2bb'
+down_revision = '02bb8d55588c'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        'users',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('alternative_id', sa.Integer(), nullable=False),
+        sa.Column('name', sa.String(length=100), nullable=False),
+        sa.Column('email', sa.String(length=40), nullable=False),
+        sa.Column('password', sa.String(length=200), nullable=False),
+        sa.Column('created_on', sa.DateTime(timezone=True), nullable=False),
+        sa.Column('last_login', sa.DateTime(timezone=True), nullable=True),
+        sa.Column('preferences', postgresql.JSONB(astext_type=sa.Text()), nullable=False),
+        sa.PrimaryKeyConstraint('id'),
+        sa.UniqueConstraint('alternative_id'),
+        sa.UniqueConstraint('email'),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table('users')
