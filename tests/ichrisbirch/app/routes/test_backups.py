6ae94802ddb93602dce6c035396ce0dedd9c676e
Chris Birch <datapointchris@gmail.com>
2024-05-14 09:58:02 -0400

    fix(test): skip test for backups in github actions because aws credentials not available

--- tests/ichrisbirch/app/routes/test_backups.py
+++ tests/ichrisbirch/app/routes/test_backups.py
@@ -1,8 +1,15 @@
+import os
+
+import pytest
 from fastapi import status
 
 from tests.helpers import show_status_and_response
 
 
+@pytest.mark.skipif(
+    os.environ.get('GITHUB_ACTIONS') == 'true',
+    reason='AWS credentials not available on Github Actions, no plans to implement',
+)
 def test_get_serverstats(test_app):
     response = test_app.get('/admin/backups/')
     assert response.status_code == status.HTTP_200_OK, show_status_and_response(response)
