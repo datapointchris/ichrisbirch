477a08ef7c52bd15e120af753c0be33e3f266506
Chris Birch <datapointchris@gmail.com>
2024-05-16 07:00:18 -0400

    fix(tests): jobs that require session take as a parameter instead of SessionLocal

--- tests/ichrisbirch/scheduler/test_jobs.py
+++ tests/ichrisbirch/scheduler/test_jobs.py
@@ -2,12 +2,14 @@ import pytest
 
 import tests.util
 from ichrisbirch.scheduler import jobs
+from tests.util import SessionTesting
 from tests.util import show_status_and_response
 
 
 @pytest.fixture(autouse=True)
 def insert_testing_data():
-    tests.util.insert_test_data('tasks', 'autotasks')
+    tests.util.insert_test_data('tasks')
+    tests.util.insert_test_data('autotasks')
 
 
 def test_decrease_task_priority(test_api):
@@ -15,11 +17,11 @@ def test_decrease_task_priority(test_api):
     before = test_api.get('/tasks/')
     assert before.status_code == 200, show_status_and_response(before)
     assert sum([task['priority'] for task in before.json()]) == 5 + 10 + 15
-    jobs.decrease_task_priority()
+    jobs.decrease_task_priority(session=SessionTesting)
     after = test_api.get('/tasks/')
     assert after.status_code == 200, show_status_and_response(after)
     # Task 3 is completed so it's priority should not be decreased from 15
-    assert sum([task['priority'] for task in after.json()]) == 4 + 9 + 15
+    assert sum([task['priority'] for task in after.json()]) == 4 + 9 + 15, after.json()
     assert len(before.json()) == len(after.json())
 
 
@@ -28,7 +30,7 @@ def test_check_and_run_autotasks(test_api):
     before = test_api.get('/tasks/')
     assert before.status_code == 200, show_status_and_response(before)
     assert len(before.json()) == 3
-    jobs.check_and_run_autotasks()
+    jobs.check_and_run_autotasks(session=SessionTesting)
     after = test_api.get('/tasks/')
     assert after.status_code == 200, show_status_and_response(after)
-    assert len(after.json()) == 6
+    assert len(after.json()) == 6, after.json()
