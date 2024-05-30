90a0c19e7c68358a3e976e20a8dff930d220af55
Chris Birch <datapointchris@gmail.com>
2024-05-30 00:19:54 -0400

    test: refactor conftest to make more clear the flow of test environment setup

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,6 @@ from typing import Generator
 import pytest
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.schedulers.blocking import BlockingScheduler
-from docker.models.containers import Container
 from fastapi.testclient import TestClient
 from flask.testing import FlaskClient
 from flask_login import FlaskLoginClient
@@ -26,7 +25,6 @@ from ichrisbirch.scheduler.main import get_jobstore
 from tests import test_data
 
 logger = logging.getLogger(__name__)
-docker_logger = logging.getLogger('DOCKER')
 
 settings = get_settings('testing')
 logger.info(f"load settings from environment: {settings.ENVIRONMENT}")
@@ -48,7 +46,7 @@ def test_app() -> Generator[FlaskClient, Any, None]:
     app.config.update({'WTF_CSRF_ENABLED': False})
     with app.test_client() as client:
         with app.app_context():
-            client.delete_cookie('session')
+            # client.delete_cookie('session')
             yield client
 
 
@@ -94,8 +92,10 @@ def create_and_drop_tables():
     This is the easiest way to ensure a clean db each time a new test is run.
     """
     Base.metadata.create_all(tests.util.ENGINE)
+    logger.info('created all tables')
     yield
     Base.metadata.drop_all(tests.util.ENGINE)
+    logger.info('dropped all tables')
 
 
 @pytest.fixture(scope='function')
@@ -126,52 +126,77 @@ def setup_test_environment():
     data to run test =>c Drop all tables after test completes => Control back to setup_test_environment => Stop Postgres
     container => Kill Postgres, Uvicorn, and Gunicorn threads
     """
-    docker_client = tests.util.get_docker_client()
-    postgres_container_config = dict(
-        image='postgres:16',
-        name='postgres_testing',
-        environment={
-            'ENVIRONMENT': settings.ENVIRONMENT,
-            'POSTGRES_USER': settings.postgres.username,
-            'POSTGRES_PASSWORD': settings.postgres.password,
-            'POSTGRES_DB': settings.postgres.database,
-        },
-        ports=[5432],
-        # Bind to port 5434 on host machine, so that it doesn't conflict with local Postgres
-        host_config=docker_client.create_host_config(port_bindings={5432: 5434}, auto_remove=True),
-        detach=True,
-        labels=['testing'],
-    )
-    # Create Postgres Docker container
-    postgres_container = tests.util.create_docker_container(client=docker_client, config=postgres_container_config)
 
-    def stream_docker_container_logs(container: Container):
-        for log in docker_client.logs(container=container.get('Id'), stream=True, follow=True):
-            log = log.decode().strip()
-            docker_logger.info(log)
+    try:
+        docker_client = tests.util.get_docker_client(logger=logger)
+        postgres_container = tests.util.create_postgres_docker_container(client=docker_client)
+        logger.info('created postgres docker container')
+        postgres_container_id = postgres_container.get('Id')
+        postgres_thread = threading.Thread(target=docker_client.start, kwargs={'container': postgres_container_id})
+        postgres_thread.start()
+        time.sleep(3)  # Allow Postgres in Docker to start
+        logger.info('started postgres docker container thread')
 
-    # Start Docker log stream in its own thread
-    docker_log_thread = threading.Thread(
-        target=stream_docker_container_logs,
-        kwargs={'container': postgres_container},
-    )
+        docker_log_thread = threading.Thread(
+            target=tests.util.docker_logs, kwargs={'client': docker_client, 'container_id': postgres_container_id}
+        )
+        docker_log_thread.start()
+        logger.info('started docker log stream thread')
 
-    # Start Postgres container in its own thread
-    postgres_thread = threading.Thread(
-        target=docker_client.start,
-        kwargs={'container': postgres_container.get('Id')},
-    )
-    postgres_thread.start()
-    # Allow Postgres time to start
-    time.sleep(3)
-    docker_log_thread.start()
+        _create_database_schemas(schemas=settings.DB_SCHEMAS, session=tests.util.SessionTesting)
+        logger.info('created database schemas')
 
-    # Create Schemas
-    # with next(tests.helpers.get_testing_session()) as session:
-    with tests.util.SessionTesting() as session:
-        for schema_name in settings.DB_SCHEMAS:
+        # Copy current environment and set ENVIRONMENT to testing for subprocesses
+        testing_env = os.environ | {'ENVIRONMENT': 'testing'}
+
+        api_uvicorn_process = _create_api_uvicorn_process(env=testing_env)
+        api_uvicorn_thread = threading.Thread(target=api_uvicorn_process.wait)
+        api_uvicorn_thread.start()
+        time.sleep(2)  # Allow Uvicorn FastAPI to start
+        logger.info('started api uvicorn process thread')
+
+        app_gunicorn_process = _create_app_gunicorn_process(env=testing_env)
+        app_gunicorn_thread = threading.Thread(target=app_gunicorn_process.wait)
+        app_gunicorn_thread.start()
+        time.sleep(2)  # Allow Flask to start
+        logger.info('started app gunicorn process thread')
+
+        yield  # hold on while all tests in session run
+
+    except Exception as e:
+        logger.error(f'failed to set up the test environment: {e}')
+
+    finally:
+        # No need to delete the database, as it is in a Docker container
+        # Stop container and join thread to main thread
+        logger.info(f'stopping postgres docker container: {postgres_container_id}')
+        docker_client.stop(container=postgres_container_id)
+
+        logger.info('stopping postgres docker thread')
+        postgres_thread.join()
+
+        logger.info('stopping docker logging thread')
+        docker_log_thread.join()
+
+        logger.info('stopping api uvicorn process')
+        api_uvicorn_process.kill()
+
+        logger.info('stopping api uvicorn thread')
+        api_uvicorn_thread.join()
+
+        logger.info('stopping app gunicorn process')
+        app_gunicorn_process.kill()
+
+        logger.info('stopping app gunicorn thread')
+        app_gunicorn_thread.join()
+
+
+def _create_database_schemas(schemas, session):
+    with session() as session:
+        for schema_name in schemas:
             try:
                 session.execute(CreateSchema(schema_name))
+                logger.debug(f'created schema {schema_name}')
             except Exception as e:
                 logger.error(f"Failed to create schema: {e}")
                 debug_message = f"""Failed to create schema: {e}
@@ -186,11 +211,9 @@ def setup_test_environment():
                 pytest.exit(debug_message)
         session.commit()
 
-    # Copy current environment and set ENVIRONMENT to testing for subprocesses
-    testing_env = os.environ | {'ENVIRONMENT': settings.ENVIRONMENT}
 
-    # Start Uvicorn API (FastAPI) subprocess in its own thread (for testing APP, that needs an API response)
-    # This is easier than mocking everything
+def _create_api_uvicorn_process(env):
+    """Start Uvicorn API (FastAPI) subprocess in its own thread This is easier than mocking everything."""
     api_uvicorn_command = ' '.join(
         [
             'poetry run uvicorn ichrisbirch.wsgi_api:api',
@@ -199,12 +222,12 @@ def setup_test_environment():
             '--log-level debug',
         ]
     )
-    api_uvicorn_process = subprocess.Popen(api_uvicorn_command.split(), env=testing_env)
-    api_uvicorn_thread = threading.Thread(target=api_uvicorn_process.wait)
-    api_uvicorn_thread.start()
-    time.sleep(1)  # Allow Uvicorn FastAPI to start
+    process = subprocess.Popen(api_uvicorn_command.split(), env=env)
+    return process
 
-    # Start Gunicorn App (Flask) subprocess in its own thread (for testing the frontend)
+
+def _create_app_gunicorn_process(env):
+    """Start Gunicorn App (Flask) subprocess in its own thread."""
     app_gunicorn_command = ' '.join(
         [
             'poetry run gunicorn ichrisbirch.wsgi_app:app',
@@ -212,22 +235,5 @@ def setup_test_environment():
             '--log-level DEBUG',
         ]
     )
-    app_gunicorn_process = subprocess.Popen(app_gunicorn_command.split(), env=testing_env)
-    app_gunicorn_thread = threading.Thread(target=app_gunicorn_process.wait)
-    app_gunicorn_thread.start()
-    time.sleep(1)  # Allow Flask to start
-
-    try:
-        yield  # hold on while all tests in session run
-    finally:
-        # No need to delete the database, as it is in a Docker container
-        # Stop container and join thread to main thread
-        docker_client.stop(container=postgres_container.get('Id'))
-        postgres_thread.join()
-        docker_log_thread.join()
-        # Kill uvicorn process and join thread to main thread
-        api_uvicorn_process.kill()
-        api_uvicorn_thread.join()
-        # Kill flask process and join thread to main thread
-        app_gunicorn_process.kill()
-        app_gunicorn_thread.join()
+    process = subprocess.Popen(app_gunicorn_command.split(), env=env)
+    return process
--- tests/util.py
+++ tests/util.py
@@ -22,55 +22,20 @@ logger = logging.getLogger(__name__)
 settings = get_settings('testing')
 logger.info(f"load settings from environment: {settings.ENVIRONMENT}")
 
-ENGINE = create_engine(settings.sqlalchemy.db_uri, echo=False, future=True)
-SessionTesting = sessionmaker(bind=ENGINE, autocommit=False, autoflush=True, future=True, expire_on_commit=False)
 
-
-def show_status_and_response(response: httpx.Response) -> dict[str, str]:
-    """Convert status code to description and return response if any."""
-    d = {}
-    for attr in dir(status):
-        code = attr.split('_')[1]
-        d[int(code)] = attr
-    try:
-        content = response.json()
-    except (httpx.DecodingError, TypeError) as e:
-        logger.warning(f'error decoding response content: {e}')
-        content = '<no response content>'
-
-    return {d.get(response.status_code, 'UNKNOWN'): content}
-
-
-def get_docker_client() -> docker.APIClient:
-    try:
-        return docker.APIClient(base_url='unix://var/run/docker.sock')
-    except DockerException:
-        # Docker is not running
-        print('docker is not running, starting...')
-        subprocess.run(['open', '-ga', 'docker'])
-        subprocess.run(['sleep', '20'])
-        print('docker started')
-        # Try again, let failure surface in the next command if this was not the issue
-        return docker.APIClient(base_url='unix://var/run/docker.sock')
-
-
-def create_docker_container(client: docker.APIClient, config: dict[str, Any]) -> Container:
-    image = config.pop("image")
-    try:
-        client.pull(image)
-        container = client.create_container(image, **config)
-    except DockerException as e:
-        message = f"Failed to CREATE Docker client: {e}"
-        logger.error(message)
-        try:
-            client.remove_container(container=config.get("name"), force=True)
-            container = client.create_container(image, **config)
-        except DockerException as e:
-            message = f"Failed to REMOVE and RECREATE Docker container: {e}"
-            logger.error(message)
-            pytest.exit(message)
-    time.sleep(3)  # Must sleep to allow creation of detached Docker container
-    return container
+ENGINE = create_engine(
+    settings.sqlalchemy.db_uri,
+    echo=False,
+    pool_pre_ping=True,
+    pool_recycle=5,
+    connect_args={
+        "keepalives": 1,
+        "keepalives_idle": 30,
+        "keepalives_interval": 10,
+        "keepalives_count": 5,
+    },
+)
+SessionTesting = sessionmaker(bind=ENGINE, autocommit=False, autoflush=True, expire_on_commit=False)
 
 
 def get_testing_session() -> Generator[Session, None, None]:
@@ -113,7 +78,81 @@ def insert_test_data(*datasets) -> None:
         'users': tests.test_data.users.BASE_DATA,
     }
     selected_datasets = [deepcopy(base_datasets[key]) for key in datasets if key in base_datasets]
+    logger.info(f'inserting testing dataset: {' '.join(f"'{d}'" for d in datasets)}')
     with SessionTesting() as session:
         for data in selected_datasets:
             session.add_all(data)
         session.commit()
+
+
+def show_status_and_response(response: httpx.Response) -> dict[str, str]:
+    """Convert status code to description and return response if any."""
+    d = {}
+    for attr in dir(status):
+        code = attr.split('_')[1]
+        d[int(code)] = attr
+    try:
+        content = response.json()
+    except (httpx.DecodingError, TypeError) as e:
+        logger.warning(f'error decoding response content: {e}')
+        content = '<no response content>'
+
+    return {d.get(response.status_code, 'UNKNOWN'): content}
+
+
+def get_docker_client(logger: logging.Logger) -> docker.APIClient:
+    try:
+        return docker.APIClient(base_url='unix://var/run/docker.sock')
+    except DockerException:
+        logger.warning('docker is not running, starting...')
+        subprocess.run(['open', '-ga', 'docker'])
+        subprocess.run(['sleep', '20'])
+        logger.info('docker started')
+        # Try again, let failure surface in the next command if this was not the issue
+        return docker.APIClient(base_url='unix://var/run/docker.sock')
+
+
+def create_docker_container(client: docker.APIClient, config: dict[str, Any]) -> Container:
+    image = config.pop("image")
+    try:
+        client.pull(image)
+        container = client.create_container(image, **config)
+    except DockerException as e:
+        message = f"Failed to CREATE Docker container: {e}"
+        logger.error(message)
+        try:
+            client.remove_container(container=config.get("name"), force=True)
+            container = client.create_container(image, **config)
+        except DockerException as e:
+            message = f"Failed to REMOVE and RECREATE Docker container: {e}"
+            logger.error(message)
+            pytest.exit(message)
+    time.sleep(3)  # Must sleep to allow creation of detached Docker container
+    return container
+
+
+def create_postgres_docker_container(client: docker.APIClient):
+    postgres_container_config = dict(
+        image='postgres:16',
+        name='postgres_testing',
+        environment={
+            'ENVIRONMENT': settings.ENVIRONMENT,
+            'POSTGRES_USER': settings.postgres.username,
+            'POSTGRES_PASSWORD': settings.postgres.password,
+            'POSTGRES_DB': settings.postgres.database,
+        },
+        ports=[5432],
+        # Bind to port 5434 on host machine, so that it doesn't conflict with local Postgres
+        host_config=client.create_host_config(port_bindings={5432: 5434}, auto_remove=True),
+        detach=True,
+        labels=['testing'],
+    )
+    postgres_container = create_docker_container(client=client, config=postgres_container_config)
+    return postgres_container
+
+
+def docker_logs(client: docker.APIClient, container_id: str):
+    docker_logger = logging.getLogger('DOCKER')
+    for log in client.logs(container=container_id, stream=True, follow=True):
+        log = log.decode().strip()
+        docker_logger.info(log)
