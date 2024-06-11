8bd69ea9bb997c3c4d6ffae1e9c8e08d9811a4a6
Chris Birch <datapointchris@gmail.com>
2024-06-11 12:23:48 -0400

    ops: add script to create log files on new ec2 server

---
+++ scripts/make_log_files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+LOG_DIR="/var/log/ichrisbirch"
+sudo mkdir "$LOG_DIR"
+sudo chown ubuntu "$LOG_DIR"
+touch "$LOG_DIR/ichrisbirch.log"
+touch "$LOG_DIR/app.log"
+touch "$LOG_DIR/api.log"
+touch "$LOG_DIR/scheduler.log"
+touch "$LOG_DIR/ichrisbirch.json"
