711b97cd532e0ba8e51d2c3a28305c29b272c1c6
Chris Birch <datapointchris@gmail.com>
2024-07-30 00:08:51 -0400

    fix(tests/admin): cannot use backups endpoint for tests because no AWS access in github actions

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -1,40 +1,45 @@
+ALL_ACCESS = '/'
+USER_ACCESS = '/users/profile/'
+ADMIN_ACCESS = '/admin/'
+
+
 class TestHomeAccess:
     def test_anonymous_access_home(self, test_app_function):
-        response = test_app_function.get('/')
+        response = test_app_function.get(ALL_ACCESS)
         assert response.status_code == 200
 
     def test_login_access_home(self, test_app_logged_in_function):
-        response = test_app_logged_in_function.get('/')
+        response = test_app_logged_in_function.get(ALL_ACCESS)
         assert response.status_code == 200
 
     def test_admin_access_home(self, test_app_logged_in_admin_function):
-        response = test_app_logged_in_admin_function.get('/')
+        response = test_app_logged_in_admin_function.get(ALL_ACCESS)
         assert response.status_code == 200
 
 
 class TestProfileAccess:
     def test_anonymous_no_access_profile(self, test_app_function):
-        response = test_app_function.get('/users/profile/')
+        response = test_app_function.get(USER_ACCESS)
         assert response.status_code != 200
 
     def test_login_access_profile(self, test_app_logged_in_function):
-        response = test_app_logged_in_function.get('/users/profile/')
+        response = test_app_logged_in_function.get(USER_ACCESS)
         assert response.status_code == 200
 
     def test_admin_access_profile(self, test_app_logged_in_admin_function):
-        response = test_app_logged_in_admin_function.get('/admin/backups/')
+        response = test_app_logged_in_admin_function.get(USER_ACCESS)
         assert response.status_code == 200
 
 
 class TestAdminAccess:
     def test_anonymous_no_access_admin(self, test_app_function):
-        response = test_app_function.get('/admin/backups/')
+        response = test_app_function.get(ADMIN_ACCESS)
         assert response.status_code != 200
 
     def test_no_admin_no_access_admin(self, test_app_logged_in_function):
-        response = test_app_logged_in_function.get('/admin/backups/')
+        response = test_app_logged_in_function.get(ADMIN_ACCESS)
         assert response.status_code != 200
 
     def test_admin_access_admin(self, test_app_logged_in_admin_function):
-        response = test_app_logged_in_admin_function.get('/admin/backups/')
+        response = test_app_logged_in_admin_function.get(ADMIN_ACCESS)
         assert response.status_code == 200
