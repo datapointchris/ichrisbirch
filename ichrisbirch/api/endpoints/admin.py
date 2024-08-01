86bcd8825bd74d2957cab0a23621087ed1cf5774
Chris Birch <datapointchris@gmail.com>
2024-07-31 22:43:41 -0400

    refactor(logs): abstact getting log filename from logger handler into utility function

--- ichrisbirch/api/endpoints/admin.py
+++ ichrisbirch/api/endpoints/admin.py
@@ -6,24 +6,24 @@ from fastapi import APIRouter
 from fastapi import WebSocket
 from pygtail import Pygtail
 
+from ichrisbirch.util import get_logger_filename_from_handlername
+
 logger = logging.getLogger('api.admin')
 router = APIRouter()
 
 log_queue: queue.Queue = queue.Queue()
 
-if handler := logging.getHandlerByName('ichrisbirch_file'):
-    if isinstance(handler, logging.FileHandler):
-        filename = handler.baseFilename
+log_filename = get_logger_filename_from_handlername('ichrisbirch_file')  # noqa: FURB120
 
 
 async def read_new_logs():
-    for line in Pygtail(filename, paranoid=True):
+    for line in Pygtail(log_filename, paranoid=True):
         log_queue.put(line)
 
 
 @router.websocket('/log-stream/')
 async def websocket_endpoint_log(websocket: WebSocket):
-    logger.debug(f'logstream source: {filename}')
+    logger.debug(f'logstream source: {log_filename}')
     logger.debug(f'websocket: {websocket}')
     logger.debug(f'websocket: {websocket.base_url}')
     await websocket.accept()
--- ichrisbirch/util.py
+++ ichrisbirch/util.py
@@ -1,3 +1,4 @@
+import logging
 from pathlib import Path
 
 
@@ -13,3 +14,10 @@ def find_project_root(
     if parent_directory == directory:
         raise FileNotFoundError(f'Could not find project root directory searching for {target_file}')
     return find_project_root(parent_directory)
+
+
+def get_logger_filename_from_handlername(handler_name: str = 'ichrisbirch_file') -> str | None:
+    if handler := logging.getHandlerByName(handler_name):
+        if isinstance(handler, logging.FileHandler):
+            return handler.baseFilename
+    return None
