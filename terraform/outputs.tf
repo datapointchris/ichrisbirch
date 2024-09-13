3f5c026bc6ddb7f1ea72b0c5fc8376ef17eed85f
Chris Birch <datapointchris@gmail.com>
2024-09-13 19:09:53 -0400

    feat(iac/route53): update route53 records to get dynamic nameservers instead of assigning in code

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
+# ------------------------------------------------------------------ #
+# DATABASE
+# ------------------------------------------------------------------ #
+
 output "db_endpoint" {
   value = aws_db_instance.ichrisbirch.endpoint
 }
@@ -26,56 +30,16 @@ output "dynamodb_terraform_state_locking_table_id" {
   value = aws_dynamodb_table.ichrisbirch_terraform_state_locking.id
 }
 
-output "webserver_ami_id" {
-  value = data.aws_ami.ichrisbirch_t3medium_2vcpu_4gb_py312.id
-}
+# ------------------------------------------------------------------ #
+# NETWORK
+# ------------------------------------------------------------------ #
 
-output "webserver_availability_zone" {
-  value = aws_instance.ichrisbirch_webserver.availability_zone
-}
-
-output "webserver_iam_instance_profile" {
-  value = aws_instance.ichrisbirch_webserver.iam_instance_profile
-}
-
-output "webserver_id" {
-  value = aws_instance.ichrisbirch_webserver.id
-}
-
-output "webserver_instance_type" {
-  value = aws_instance.ichrisbirch_webserver.instance_type
-}
-
-output "webserver_key_name" {
-  value = aws_instance.ichrisbirch_webserver.key_name
-}
-
-output "webserver_security_groups" {
-  value = join(", ", aws_instance.ichrisbirch_webserver.vpc_security_group_ids[*])
-}
-
-output "webserver_subnet_id" {
-  value = aws_instance.ichrisbirch_webserver.subnet_id
-}
-
-output "webserver_route_table_id" {
-  value = aws_route_table.prod_public.id
-}
-
-output "webserver_elastic_ip" {
-  value = aws_eip.ichrisbirch_elastic_ip.public_ip
+output "internet_gateway_id" {
+  value = aws_internet_gateway.prod.id
 }
 
 output "network_interface_ichrisbirch_webserver_id" {
   value = aws_network_interface.ichrisbirch_webserver.id
-}
-
-output "security_group_ichrisbirch_webserver_id" {
-  value = aws_security_group.ichrisbirch_webserver.id
-}
-
-output "internet_gateway_id" {
-  value = aws_internet_gateway.prod.id
 }
 
 output "prod_private_route_table_id" {
@@ -102,7 +66,267 @@ output "prod_vpc_id" {
   value = aws_vpc.prod.id
 }
 
+output "security_group_ichrisbirch_webserver_id" {
+  value = aws_security_group.ichrisbirch_webserver.id
+}
+
+# ------------------------------------------------------------------ #
+# WEBSERVER
+# ------------------------------------------------------------------ #
+
+output "webserver_ami_id" {
+  value = data.aws_ami.ichrisbirch_t3medium_2vcpu_4gb_py312.id
+}
+
+output "webserver_availability_zone" {
+  value = aws_instance.ichrisbirch_webserver.availability_zone
+}
+
+output "webserver_elastic_ip" {
+  value = aws_eip.ichrisbirch_elastic_ip.public_ip
+}
+
+output "webserver_iam_instance_profile" {
+  value = aws_instance.ichrisbirch_webserver.iam_instance_profile
+}
+
+output "webserver_id" {
+  value = aws_instance.ichrisbirch_webserver.id
+}
+
+output "webserver_instance_type" {
+  value = aws_instance.ichrisbirch_webserver.instance_type
+}
+
+output "webserver_key_name" {
+  value = aws_instance.ichrisbirch_webserver.key_name
+}
+
+output "webserver_route_table_id" {
+  value = aws_route_table.prod_public.id
+}
+
+output "webserver_security_groups" {
+  value = join(", ", aws_instance.ichrisbirch_webserver.vpc_security_group_ids[*])
+}
+
+output "webserver_subnet_id" {
+  value = aws_instance.ichrisbirch_webserver.subnet_id
+}
+
+# ------------------------------------------------------------------ #
+# IAM
+# ------------------------------------------------------------------ #
+
+# ---------- IAM ROLES ---------- #
+
+output "iam_role_admin_arn" {
+  value = aws_iam_role.admin.arn
+}
+
+output "iam_role_admin_name" {
+  value = aws_iam_role.admin.name
+}
+
+output "iam_role_ichrisbirch_webserver_arn" {
+  value = aws_iam_role.ichrisbirch_webserver.arn
+}
+
+output "iam_role_ichrisbirch_webserver_name" {
+  value = aws_iam_role.ichrisbirch_webserver.name
+}
+
+# ---------- IAM POLICIES ---------- #
+
+output "iam_policy_access_webserver_keys_arn" {
+  value = aws_iam_policy.access_webserver_keys.arn
+}
+
+output "iam_policy_access_webserver_keys_name" {
+  value = aws_iam_policy.access_webserver_keys.name
+}
+
+output "iam_policy_allow_pass_webserver_role_arn" {
+  value = aws_iam_policy.allow_pass_webserver_role.arn
+}
+
+output "iam_policy_allow_pass_webserver_role_name" {
+  value = aws_iam_policy.allow_pass_webserver_role.name
+}
+
+output "iam_policy_assume_admin_role_arn" {
+  value = aws_iam_policy.assume_admin_role.arn
+}
+
+output "iam_policy_assume_admin_role_name" {
+  value = aws_iam_policy.assume_admin_role.name
+}
+
+output "iam_policy_cloud_developer_arn" {
+  value = aws_iam_policy.cloud_developer.arn
+}
+
+output "iam_policy_cloud_developer_name" {
+  value = aws_iam_policy.cloud_developer.name
+}
+
+output "iam_policy_ec2_instance_connect_arn" {
+  value = aws_iam_policy.ec2_instance_connect.arn
+}
+
+output "iam_policy_ec2_instance_connect_name" {
+  value = aws_iam_policy.ec2_instance_connect.name
+}
+
+# ---------- IAM INSTANCE PROFILES ---------- #
+
+output "iam_instance_profile_ichrisbirch_webserver_arn" {
+  value = aws_iam_instance_profile.ichrisbirch_webserver.arn
+}
+
+output "iam_instance_profile_ichrisbirch_webserver_name" {
+  value = aws_iam_instance_profile.ichrisbirch_webserver.name
+}
+
+# ---------- IAM GROUPS ---------- #
+
+output "iam_group_developer_arn" {
+  value = aws_iam_group.developer.arn
+}
+
+output "iam_group_developer_name" {
+  value = aws_iam_group.developer.name
+}
+
+# ---------- IAM USERS ---------- #
+
+output "iam_user_chris_birch_arn" {
+  value = aws_iam_user.chris_birch.arn
+}
+
+output "iam_user_chris_birch_name" {
+  value = aws_iam_user.chris_birch.name
+}
+
+output "iam_user_john_kundycki_arn" {
+  value = aws_iam_user.john_kundycki.arn
+}
+
+output "iam_user_john_kundycki_name" {
+  value = aws_iam_user.john_kundycki.name
+}
+
 output "user_chris_birch_generated_password" {
   value     = aws_iam_user_login_profile.chris_birch.password
   sensitive = true
+}
+
+# ------------------------------------------------------------------ #
+# ROUTE 53
+# ------------------------------------------------------------------ #
+
+# ---------- Main Site ---------- #
+
+output "route53_api_ichrisbirch_ns_ns_fqdn" {
+  value = aws_route53_record.api_ichrisbirch_ns_ns.fqdn
+}
+
+output "route53_api_ichrisbirch_ns_ns_name" {
+  value = aws_route53_record.api_ichrisbirch_ns_ns.name
+}
+
+output "route53_ichrisbirch_ns_fqdn" {
+  value = aws_route53_record.ichrisbirch_ns.fqdn
+}
+
+output "route53_ichrisbirch_ns_name" {
+  value = aws_route53_record.ichrisbirch_ns.name
+}
+
+output "route53_ichrisbirch_zone_id" {
+  value = aws_route53_zone.ichrisbirch.zone_id
+}
+
+output "route53_ichrisbirch_zone_name_servers" {
+  value = aws_route53_zone.ichrisbirch.name_servers
+}
+
+output "route53_ichrisbirch_zone_primary_name_server" {
+  value = aws_route53_zone.ichrisbirch.primary_name_server
+}
+
+output "route53_www_ichrisbirch_a_fqdn" {
+  value = aws_route53_record.www_ichrisbirch_a.fqdn
+}
+
+output "route53_www_ichrisbirch_a_name" {
+  value = aws_route53_record.www_ichrisbirch_a.name
+}
+
+# ---------- API Subdomain ---------- #
+
+output "route53_api_ichrisbirch_a_fqdn" {
+  value = aws_route53_record.api_ichrisbirch_a.fqdn
+}
+
+output "route53_api_ichrisbirch_a_name" {
+  value = aws_route53_record.api_ichrisbirch_a.name
+}
+
+output "route53_api_ichrisbirch_ns_fqdn" {
+  value = aws_route53_record.api_ichrisbirch_ns.fqdn
+}
+
+output "route53_api_ichrisbirch_ns_name" {
+  value = aws_route53_record.api_ichrisbirch_ns.name
+}
+
+output "route53_api_ichrisbirch_zone_id" {
+  value = aws_route53_zone.api_ichrisbirch.zone_id
+}
+
+output "route53_api_ichrisbirch_zone_name_servers" {
+  value = aws_route53_zone.api_ichrisbirch.name_servers
+}
+
+output "route53_api_ichrisbirch_zone_primary_name_server" {
+  value = aws_route53_zone.api_ichrisbirch.primary_name_server
+}
+
+# ---------- Docs Site ---------- #
+
+output "route53_docs_ichrisbirch_zone_id" {
+  value = aws_route53_zone.docs_ichrisbirch.zone_id
+}
+
+output "route53_docs_ichrisbirch_zone_name_servers" {
+  value = aws_route53_zone.docs_ichrisbirch.name_servers
+}
+
+output "route53_docs_ichrisbirch_zone_primary_name_server" {
+  value = aws_route53_zone.docs_ichrisbirch.primary_name_server
+}
+
+output "route53_docs_ichrisbirch_ns_name" {
+  value = aws_route53_record.docs_ichrisbirch_ns.name
+}
+
+output "route53_docs_ichrisbirch_ns_fqdn" {
+  value = aws_route53_record.docs_ichrisbirch_ns.fqdn
+}
+
+output "route53_docs_ichrisbirch_a_name" {
+  value = aws_route53_record.docs_ichrisbirch_a.name
+}
+
+output "route53_docs_ichrisbirch_a_fqdn" {
+  value = aws_route53_record.docs_ichrisbirch_a.fqdn
+}
+
+output "route53_docs_ichrisbirch_cname_name" {
+  value = aws_route53_record.docs_ichrisbirch_cname.name
+}
+
+output "route53_docs_ichrisbirch_cname_fqdn" {
+  value = aws_route53_record.docs_ichrisbirch_cname.fqdn
 }
--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,57 +1,140 @@
+
 # ---------- Main Site ---------- #
 
-resource "aws_route53_zone" "ichrisbirch_com" {
+resource "aws_route53_zone" "ichrisbirch" {
+  name          = "ichrisbirch.com"
   comment       = "Main Site"
   force_destroy = false
-  name          = "ichrisbirch.com"
 }
 
-resource "aws_route53_record" "www_ichrisbirch_com_a" {
+resource "aws_route53_record" "www_ichrisbirch_a" {
   name    = "www.ichrisbirch.com"
+  zone_id = aws_route53_zone.ichrisbirch.zone_id
+  # ttl     = 300
+  ttl     = 30
+  type    = "A"
   records = [aws_eip.ichrisbirch_elastic_ip.public_ip]
-  ttl     = 300
-  type    = "A"
-  zone_id = aws_route53_zone.ichrisbirch_com.zone_id
 }
 
-resource "aws_route53_record" "docs_ichrisbirch_com_cname" {
-  name    = "docs.ichrisbirch.com"
-  records = ["datapointchris.github.io"]
-  ttl     = 300
-  type    = "CNAME"
-  zone_id = aws_route53_zone.ichrisbirch_com.zone_id
+# resource "aws_route53_record" "ichrisbirch_soa" {
+#   name    = "ichrisbirch.com"
+#   zone_id = aws_route53_zone.ichrisbirch.zone_id
+#   # ttl             = 900
+#   ttl             = 30
+#   type            = "SOA"
+#   allow_overwrite = true
+#   records         = ["ns-721.awsdns-26.net. awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400"]
+# }
+
+resource "aws_route53_record" "ichrisbirch_ns" {
+  name    = "ichrisbirch.com"
+  zone_id = aws_route53_zone.ichrisbirch.zone_id
+  # ttl             = 172800
+  ttl             = 30
+  type            = "NS"
+  allow_overwrite = true
+  records         = aws_route53_zone.ichrisbirch.name_servers
 }
 
-
-# ---------- Docs Site ---------- #
-
-resource "aws_route53_zone" "docs_ichrisbirch_com" {
-  comment       = "Documentation Site"
-  force_destroy = false
-  name          = "docs.ichrisbirch.com"
-}
-
-resource "aws_route53_record" "docs_ichrisbirch_com_a" {
-  name    = "docs.ichrisbirch.com"
-  records = ["185.199.108.153", "185.199.109.153", "185.199.110.153", "185.199.111.153"]
-  ttl     = 300
-  type    = "A"
-  zone_id = aws_route53_zone.docs_ichrisbirch_com.zone_id
+resource "aws_route53_record" "api_ichrisbirch_ns_ns" {
+  name    = "api.ichrisbirch.com"
+  zone_id = aws_route53_zone.ichrisbirch.zone_id
+  # ttl             = 300
+  ttl             = 30
+  type            = "NS"
+  allow_overwrite = true
+  records         = aws_route53_zone.api_ichrisbirch.name_servers
+  depends_on      = [aws_route53_zone.api_ichrisbirch]
 }
 
 
 # ---------- API Subdomain ---------- #
 
-resource "aws_route53_zone" "api_ichrisbirch_com" {
+resource "aws_route53_zone" "api_ichrisbirch" {
+  name          = "api.ichrisbirch.com"
   comment       = "API Subdomain"
   force_destroy = false
-  name          = "api.ichrisbirch.com"
 }
 
-resource "aws_route53_record" "api_ichrisbirch_com_a" {
+resource "aws_route53_record" "api_ichrisbirch_a" {
   name    = "api.ichrisbirch.com"
-  records = [aws_eip.ichrisbirch_elastic_ip.public_ip]
-  ttl     = 300
+  zone_id = aws_route53_zone.api_ichrisbirch.zone_id
+  # ttl     = 300
+  ttl     = 30
   type    = "A"
-  zone_id = aws_route53_zone.api_ichrisbirch_com.zone_id
+  records = [aws_eip.ichrisbirch_elastic_ip.public_ip]
+}
+
+# resource "aws_route53_record" "api_ichrisbirch_soa" {
+#   name    = "api.ichrisbirch.com"
+#   zone_id = aws_route53_zone.api_ichrisbirch.zone_id
+#   # ttl             = 900
+#   ttl             = 30
+#   type            = "SOA"
+#   allow_overwrite = true
+#   records         = ["ns-115.awsdns-14.com. awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400"]
+# }
+
+resource "aws_route53_record" "api_ichrisbirch_ns" {
+  name    = "api.ichrisbirch.com"
+  zone_id = aws_route53_zone.api_ichrisbirch.zone_id
+  # ttl             = 172800
+  ttl             = 30
+  type            = "NS"
+  allow_overwrite = true
+  records         = aws_route53_zone.api_ichrisbirch.name_servers
+}
+
+
+
+
+# ---------- Docs Site ---------- #
+
+resource "aws_route53_zone" "docs_ichrisbirch" {
+  name          = "docs.ichrisbirch.com"
+  comment       = "Documentation Site"
+  force_destroy = false
+}
+
+resource "aws_route53_record" "docs_ichrisbirch_a" {
+  name    = "docs.ichrisbirch.com"
+  zone_id = aws_route53_zone.docs_ichrisbirch.zone_id
+  # ttl     = 300
+  ttl  = 30
+  type = "A"
+  records = [
+    "185.199.108.153",
+    "185.199.109.153",
+    "185.199.110.153",
+    "185.199.111.153"
+  ]
+}
+
+# resource "aws_route53_record" "docs_ichrisbirch_soa" {
+#   name    = "docs.ichrisbirch.com"
+#   zone_id = aws_route53_zone.docs_ichrisbirch.zone_id
+#   # ttl             = 900
+#   ttl             = 30
+#   type            = "SOA"
+#   allow_overwrite = true
+#   records         = ["ns-749.awsdns-29.net. awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400"]
+# }
+
+resource "aws_route53_record" "docs_ichrisbirch_ns" {
+  name    = "docs.ichrisbirch.com"
+  zone_id = aws_route53_zone.docs_ichrisbirch.zone_id
+  # ttl             = 172800
+  ttl             = 30
+  type            = "NS"
+  allow_overwrite = true
+  records         = aws_route53_zone.docs_ichrisbirch.name_servers
+}
+
+resource "aws_route53_record" "docs_ichrisbirch_cname" {
+  name    = "docs.ichrisbirch.com"
+  zone_id = aws_route53_zone.ichrisbirch.zone_id
+  # ttl     = 300
+  ttl     = 30
+  type    = "CNAME"
+  records = ["datapointchris.github.io"]
 }
