873d3f40d875e372d42783b67b7616bde2d0d871
Chris Birch <datapointchris@gmail.com>
2024-10-10 01:24:20 -0400

    build(gh-actions/terraform): debugging

--- scripts/terraform_infra_check.py
+++ scripts/terraform_infra_check.py
@@ -23,6 +23,8 @@ if EMAIL_PASSWORD:
 
 def infrastrucuture_has_changes(outfile: Path) -> bool:
     cmd = ['terraform', 'plan', '-detailed-exitcode', '-out', str(outfile)]
+    status = subprocess.getstatusoutput(['terraform', 'plan', '-detailed-exitcode'])[0]
+    print('Terraform Plan Status:', status)
     result = subprocess.run(cmd, capture_output=True, timeout=TIMEOUT)
     print()
     print('---------- Terraform Plan Result ----------')
