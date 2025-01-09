1e3f7ba1b01b6603a4bc5b454b16a68ef964ff97
Chris Birch <datapointchris@gmail.com>
2025-01-08 22:45:50 -0500

    feat(users): update all current users in db to have box_packing.compact_view default

---
+++ ichrisbirch/alembic/versions/8d06e8d736a9_add_box_packing_compact_view_to_user_.py
@@ -0,0 +1,32 @@
+"""Add box_packing.compact_view to user preferences.
+
+Revision ID: 8d06e8d736a9
+Revises: 9de0fa581d8f
+Create Date: 2025-01-08 15:07:42.999292
+"""
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = '8d06e8d736a9'
+down_revision = '9de0fa581d8f'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.execute(
+        """
+        UPDATE users
+        SET preferences = jsonb_insert(preferences::jsonb, '{box_packing}', '{"compact_view":true}'::jsonb);
+    """
+    )
+
+
+def downgrade() -> None:
+    op.execute(
+        """
+        UPDATE users
+        SET preferences = preferences #- '{box_packing,compact_view}'
+    """
+    )
