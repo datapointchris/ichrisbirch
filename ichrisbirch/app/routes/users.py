859001c005bbd68fd54a8852a6f24460ccd9456f
Chris Birch <datapointchris@gmail.com>
2025-01-08 22:31:42 -0500

    feat(users): add route to toggle box packing compact view

--- ichrisbirch/app/routes/users.py
+++ ichrisbirch/app/routes/users.py
@@ -1,7 +1,11 @@
 import logging
 
+from fastapi import status
 from flask import Blueprint
+from flask import Response
+from flask import redirect
 from flask import render_template
+from flask import request
 from flask_login import current_user
 from flask_login import fresh_login_required
 from flask_login import login_required
@@ -11,7 +15,6 @@ from ichrisbirch.app.query_api import QueryAPI
 
 logger = logging.getLogger('app.users')
 blueprint = Blueprint('users', __name__, template_folder='templates/users', static_folder='static')
-users_api = QueryAPI(base_url='users', user='', logger=logger, response_model=schemas.User)
 
 
 @blueprint.route('/profile/', methods=['GET'])
@@ -30,3 +33,19 @@ def update():
 @fresh_login_required
 def settings():
     return render_template('users/settings.html', user=current_user)
+
+
+@blueprint.route('/preferences/', methods=['POST'])
+def preferences():
+    users_api = QueryAPI(base_url='users', user='', logger=logger, response_model=schemas.User)
+
+    data = request.form.to_dict()
+    action = data.pop('action')
+
+    match action:
+        case 'toggle_box_packing_compact_view':
+            toggle = not current_user.preferences['box_packing']['compact_view']
+            users_api.patch('/me/preferences/', json={'box_packing': {'compact_view': toggle}})
+            return redirect(request.referrer)
+
+    return Response(f'Method/Action {action} not accepted', status=status.HTTP_405_METHOD_NOT_ALLOWED)
