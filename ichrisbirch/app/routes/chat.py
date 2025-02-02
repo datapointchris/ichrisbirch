c4fa5f84bd2758e0402548a8875545e78214d3aa
Chris Birch <datapointchris@gmail.com>
2025-02-02 12:39:50 -0500

    feat(chat): add chat to app frontend, using local file storage

---
+++ ichrisbirch/app/routes/chat.py
@@ -0,0 +1,22 @@
+import logging
+
+from flask import Blueprint
+from flask import redirect
+from flask_login import login_required
+
+from ichrisbirch.config import get_settings
+
+logger = logging.getLogger('app.chat')
+settings = get_settings()
+blueprint = Blueprint('chat', __name__, template_folder='templates/chat', static_folder='static')
+
+
+@blueprint.before_request
+@login_required
+def enforce_login():
+    pass
+
+
+@blueprint.route('/', methods=['GET', 'POST'])
+def index():
+    return redirect('http://localhost:8505')
