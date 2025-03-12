6b8878a23b3800d2d86b7a37346f12cac1b3748b
Chris Birch <datapointchris@gmail.com>
2025-03-12 02:05:34 -0400

    fix(chat/login): use service account and query api for credentials after api requiring auth on all endpoints

--- ichrisbirch/chat/auth.py
+++ ichrisbirch/chat/auth.py
@@ -6,17 +6,21 @@ import httpx
 import pendulum
 
 from ichrisbirch import models
+from ichrisbirch import schemas
+from ichrisbirch.app.query_api import APIServiceUser
+from ichrisbirch.app.query_api import QueryAPI
 from ichrisbirch.app.utils import url_builder
 from ichrisbirch.config import Settings
 
 logger = logging.getLogger('chat.auth')
+service_user = APIServiceUser()
 
 
 class ChatAuthClient:
     def __init__(self, settings: Settings):
         self.api_url = settings.api_url
-        self.token_url = f'{settings.api_url}/auth/token/'
-        self.users_url = f'{settings.api_url}/users/'
+        self.token_url = f'{self.api_url}/auth/token/'
+        self.users_url = f'{self.api_url}/users/'
 
     @contextmanager
     def safe_request_client(self) -> Generator[httpx.Client, None, None]:
@@ -58,18 +62,25 @@ class ChatAuthClient:
             return response.json().get("access_token")
 
     def login_username(self, username: str, password: str):
-        user_login_url = url_builder(self.users_url, 'email', username)
-        with self.safe_request_client() as client:
-            user_data = client.get(user_login_url).raise_for_status().json()
-            if user := models.User(**user_data):
-                if user.check_password(password=password):
-                    logger.debug(f'logged in user: {user.email} - last previous login: {user.last_login}')
-                    client.patch(url_builder(self.users_url, user.id), json={'last_login': pendulum.now().for_json()})
-                    return user
-                else:
-                    logger.warning(f'incorrect password for user: {user.email}')
+        users_api = QueryAPI(base_url='users', response_model=schemas.User)
+        service_user.get()
+        service_account_users_api = QueryAPI(base_url='users', response_model=schemas.User, user=service_user.user)
+
+        if user := service_account_users_api.get_one(['email', username]):
+            user = models.User(**user.model_dump())
+            if user.check_password(password):
+                logger.debug(f'logged in user: {user.name} - last previous login: {user.last_login}')
+                try:
+                    users_api = QueryAPI(base_url='users', response_model=schemas.User)
+                    users_api.patch([user.id], json={'last_login': pendulum.now().for_json()})
+                    logger.debug(f'updated last login for user: {user.name}')
+                except Exception as e:
+                    logger.error(f'error updating last login for user {user.name}: {e}')
+                return user
             else:
-                logger.warning(f'user with email {username} not found')
+                logger.warning(f'incorrect password for user: {user.email}')
+        else:
+            logger.warning(f'user with email {username} not found')
         return None
 
     def login_token(self, token: str):
