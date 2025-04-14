bad40e37a0a743bfb63f033be096d80b76cc5a86
Chris Birch <datapointchris@gmail.com>
2025-04-14 01:03:58 -0400

    build: use extra verbose for pytest in pre-commit

--- ichrisbirch/app/routes/users.py
+++ ichrisbirch/app/routes/users.py
@@ -1,5 +1,4 @@
 import logging
-from typing import Any
 
 from flask import Blueprint
 from flask import redirect
@@ -9,25 +8,12 @@ from flask_login import current_user
 from flask_login import fresh_login_required
 from flask_login import login_required
 
+from ichrisbirch import models
 from ichrisbirch import schemas
 from ichrisbirch.app.query_api import QueryAPI
 
 logger = logging.getLogger('app.users')
 blueprint = Blueprint('users', __name__, template_folder='templates/users', static_folder='static')
-
-
-def _convert_dot_separated_prefernce_to_nested_dict(dot_key: str, value: Any) -> dict:
-    """Create a nested dictionary from a dot-separated key.
-
-    Preference keys come in like:
-        `preference.app.sub_pref.key`
-    Convert to a nested dictionary compatible with User.preferences in order to store in the database as JSONB.
-    """
-    keys = dot_key.split('.')
-    nested_dict = value
-    for key in reversed(keys):
-        nested_dict = {key: nested_dict}
-    return nested_dict
 
 
 @blueprint.route('/profile/', methods=['GET'])
@@ -53,6 +39,6 @@ def settings():
 def preferences():
     users_api = QueryAPI(base_url='users', response_model=schemas.User)
     data = request.form.to_dict()
-    payload = _convert_dot_separated_prefernce_to_nested_dict(data['preference'], data['value'])
+    payload = models.User.dot_preference_to_nested_dict(data['preference'], data['value'])
     users_api.patch('/me/preferences/', json=payload)
     return redirect(request.referrer)
