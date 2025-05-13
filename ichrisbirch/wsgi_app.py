e85bc5aee3df39168195da21f6673595f0a093d0
Chris Birch <datapointchris@gmail.com>
2025-05-12 20:53:26 -0400

    refactor: use get_settings for wsgi scripts

--- ichrisbirch/wsgi_app.py
+++ ichrisbirch/wsgi_app.py
@@ -1,4 +1,4 @@
 from ichrisbirch.app.main import create_app
-from ichrisbirch.config import settings
+from ichrisbirch.config import get_settings
 
-app = create_app(settings=settings)
+app = create_app(settings=get_settings())
