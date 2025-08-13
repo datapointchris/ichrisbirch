691495864373b92fd343c8adec53dbd707aa3bea
Chris Birch <datapointchris@gmail.com>
2025-08-13 00:14:41 -0400

    refactor(scheduler): pass in settings

--- ichrisbirch/scheduler/main.py
+++ ichrisbirch/scheduler/main.py
@@ -7,21 +7,23 @@ from ichrisbirch.config import Settings
 from ichrisbirch.database.sqlalchemy.base import Base
 from ichrisbirch.scheduler.jobs import jobs_to_add
 
-logger = logging.getLogger('scheduler')
+logger = logging.getLogger(__name__)
 
 
 def get_jobstore(settings: Settings) -> SQLAlchemyJobStore:
-    """Get the jobstore for the scheduler in order to manipulate jobs in the app."""
-    return SQLAlchemyJobStore(url=settings.sqlalchemy.db_uri)
+    logger.info('Creating SQLAlchemy job store')
+    jobstore = SQLAlchemyJobStore(url=settings.sqlalchemy.db_uri, metadata=Base.metadata)
+    logger.info(f'Job store created: {jobstore}')
+    return jobstore
 
 
 def create_scheduler(settings: Settings) -> BlockingScheduler:
     logger.info('initializing')
     scheduler = BlockingScheduler()
     logger.info(f'class: {scheduler.__class__.__name__}')
-    jobstore = SQLAlchemyJobStore(url=settings.sqlalchemy.db_uri, metadata=Base.metadata)
+    jobstore = get_jobstore(settings)
     scheduler.add_jobstore(jobstore, alias='ichrisbirch', extend_existing=True)
-    logger.info(f'jobstore added: {jobstore}')
+    logger.info(f'jobstore added to scheduler: {jobstore}')
     for job in jobs_to_add:
         j = scheduler.add_job(**job.as_dict())
         logger.info(f'job added: {job.id}')
