21cbb6d30e162383d4c74405688790df581a0c55
Chris Birch <datapointchris@gmail.com>
2025-09-08 00:16:05 -0400

    chore: fix formatting for util tests

--- tests/util.py
+++ tests/util.py
@@ -17,7 +17,7 @@ def log_all_table_items(table_name, model, model_attribute=None):
     with create_session() as session:
         all_table_items = session.execute(select(model)).scalars().all()
         items = [getattr(item, model_attribute) if model_attribute else item for item in all_table_items]
-        logger.warning(f'ALL {table_name.upper()}: {', '.join(items)}')
+        logger.warning(f'ALL {table_name.upper()}: {", ".join(items)}')
 
 
 def show_status_and_response(response: httpx.Response) -> dict[str, str]:
@@ -43,7 +43,7 @@ def verify_page_title(response, title):
     """Check that the title is present in the response text."""
     # Match <title>, any whitespace/newlines, the title, and </title>
     found_title = None
-    pattern = rf"<title>\s*{re.escape(title)}\s*</title>"
+    pattern = rf'<title>\s*{re.escape(title)}\s*</title>'
     if match := re.search(pattern, response.text):
         found_title = match.group(0).removeprefix('<title>').removesuffix('</title>').strip()
     assert title == found_title, f'expected title: {title}, found: {found_title}'
